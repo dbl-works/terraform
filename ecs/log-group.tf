cdb7e4af01b0e120df58c4f521ebc059db2c00d5
Marc Qualie <marc@marcqualie.com>
2021-06-07 15:13:54 +0100

    Create ECS cluster module (#2)

---
+++ ecs/log-group.tf
@@ -0,0 +1,9 @@
+resource "aws_cloudwatch_log_group" "ecs-app" {
+  name = "/ecs/${var.project}/app/${var.environment}"
+  retention_in_days = 3
+
+  tags = {
+    Project = var.project
+    Environment = var.environment
+  }
+}
