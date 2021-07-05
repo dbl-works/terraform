286b25d2219d5db1090136b6a05bb68041d17602
swiknaba <swiknaba@users.noreply.github.com>
2021-07-05 13:40:58 +0200

    less redunant output names

--- secrets/outputs.tf
+++ secrets/outputs.tf
@@ -1,9 +1,9 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/secretsmanager_secret#attributes-reference
 
-output "secrets_id" {
+output "id" {
   value = aws_secretsmanager_secret.main.id
 }
 
-output "secrets_arn" {
+output "arn" {
   value = aws_secretsmanager_secret.main.arn
 }
