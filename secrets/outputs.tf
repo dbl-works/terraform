d9730e118b2d0f460435ffc32f00ce99976e0bbf
swiknaba <swiknaba@users.noreply.github.com>
2021-07-05 12:51:43 +0200

    Variables may not be used here.

--- secrets/outputs.tf
+++ secrets/outputs.tf
@@ -1,11 +1,9 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/secretsmanager_secret#attributes-reference
 
 output "secrets_id" {
-  value       = aws_secretsmanager_secret.main.id
-  description = "ID of the ${var.outputoutput} secrets."
+  value = aws_secretsmanager_secret.main.id
 }
 
 output "secrets_arn" {
-  value       = aws_secretsmanager_secret.main.arn
-  description = "ARN of the ${var.outputoutput} secrets."
+  value = aws_secretsmanager_secret.main.arn
 }
