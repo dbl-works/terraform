efdeb59c2844a6271932ae213131343c642f3170
swiknaba <swiknaba@users.noreply.github.com>
2021-07-05 12:37:53 +0200

    output secrets ID

--- secrets/outputs.tf
+++ secrets/outputs.tf
@@ -1,6 +1,11 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/secretsmanager_secret#attributes-reference
 
+output "secrets_id" {
+  value       = aws_secretsmanager_secret.main.id
+  description = "ID of the ${var.outputoutput} secrets."
+}
+
 output "secrets_arn" {
   value       = aws_secretsmanager_secret.main.arn
-  description = "ARN of the ${var.outputoutput} secrets. Rotation is disabled."
+  description = "ARN of the ${var.outputoutput} secrets."
 }
