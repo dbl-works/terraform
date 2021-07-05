4050f29ae965dc097b054c0db1e8b67718d106aa
swiknaba <swiknaba@users.noreply.github.com>
2021-07-05 12:20:02 +0200

    creates a secret

---
+++ secrets/secrets.tf
@@ -0,0 +1,11 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/secretsmanager_secret
+
+resource "aws_secretsmanager_secret" "main" {
+  name        = "${var.project}/${var.application}/${var.environment}"
+  description = "Secrets that are not to be stored inside ${var.application}."
+
+  tags = {
+    Project     = var.project
+    Environment = var.environment
+  }
+}
