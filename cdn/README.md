2e475ff78ee36c3a5e681aef83206207a365047b
Lud <swiknaba@users.noreply.github.com>
2021-07-08 18:46:33 +0200

    new module: CDN (#11)
    * adds all ressources for hosting a simple CDN
    
    * fix naming
    
    * fix var
    
    * fix var
    
    * output dns targe / website_endpoint
    
    * better readme
    
    * readme

---
+++ cdn/README.md
@@ -0,0 +1,24 @@
+# Terraform Module: CDN
+
+All ressources required to host a simple CDN.
+
+This will setup:
+- S3 bucket "cdn.my-project.com"
+
+## Usage
+
+```terraform
+module "cdn" {
+  source = "github.com/dbl-works/terraform//cdn?ref=v2021.07.08"
+
+  project     = local.project
+  domain_name = "my-project.com"
+
+  # Optional
+  additional_allowed_origins = [] # e.g. add "https://my-project-staging.com", "https://*.my-project-staging.com"
+}
+```
+
+
+## Outputs
+- `dns_target`, Set this as the target for your CNAME record in e.g. Cloudflare (without the `https://` prefix).
---
+++ cdn/s3.tf
@@ -0,0 +1,32 @@
+resource "aws_s3_bucket" "main" {
+  bucket = "cdn.${var.domain_name}"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = concat(["https://*.${var.domain_name}", "https://${var.domain_name}"], var.additional_allowed_origins)
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
+
+  tags = {
+    Name    = "cdn.${var.domain_name}"
+    Project = var.project
+  }
+}
