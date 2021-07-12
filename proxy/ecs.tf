b715a7a2fdb9828e0efb901566e42842a5174935
swiknaba <swiknaba@users.noreply.github.com>
2021-07-12 19:23:58 +0200

    fix ips passed to ecs

--- proxy/ecs.tf
+++ proxy/ecs.tf
@@ -1,17 +1,18 @@
 module "ecs" {
   source = "github.com/dbl-works/terraform//ecs?ref=v2021.07.09"
 
-  project            = var.project
-  environment        = var.environment
-  vpc_id             = module.vpc.id
-  subnet_private_ids = module.vpc.subnet_private_ids
-  subnet_public_ids  = module.vpc.subnet_private_ids
-  secrets_arns       = []
-  kms_key_arns       = []
-  health_check_path  = var.health_check_path
-  certificate_arn    = var.ssl_certificate_arn
-  allowlisted_ssh_ips = concat(
-    [var.cidr_block],
-    var.public_ips,
-  )
+  project             = var.project
+  environment         = var.environment
+  vpc_id              = module.vpc.id
+  subnet_private_ids  = module.vpc.subnet_private_ids
+  subnet_public_ids   = module.vpc.subnet_private_ids
+  secrets_arns        = []
+  kms_key_arns        = []
+  health_check_path   = var.health_check_path
+  certificate_arn     = var.ssl_certificate_arn
+  allowlisted_ssh_ips = [var.cidr_block]
+  # allowlisted_ssh_ips = setunion(
+  #   [var.cidr_block],
+  #   var.public_ips,
+  # )
 }
