7d896d756dfe89b0b9051b51e26ee3e9f54b495c
swiknaba <swiknaba@users.noreply.github.com>
2021-07-12 20:01:38 +0200

    fix vars

--- proxy/kms-key.tf
+++ proxy/kms-key.tf
@@ -2,8 +2,8 @@ module "kms-key" {
   source = "github.com/dbl-works/terraform//kms-key?ref=v2021.07.09"
 
   # Required
-  environment = local.environment
-  project     = local.project
+  environment = var.environment
+  project     = var.project
   alias       = "proxy"
   description = "Used for application secrets and related resources"
 }
