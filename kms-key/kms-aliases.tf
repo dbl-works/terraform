0cd6e56f301d659593c1b2d000c56a8505d12b0e
Lud <swiknaba@users.noreply.github.com>
2021-07-13 16:27:45 +0200

    auto-format all code (#17)

--- kms-key/kms-aliases.tf
+++ kms-key/kms-aliases.tf
@@ -1,4 +1,4 @@
 resource "aws_kms_alias" "a" {
-  name = "alias/${var.project}/${var.environment}/${var.alias}"
+  name          = "alias/${var.project}/${var.environment}/${var.alias}"
   target_key_id = aws_kms_key.key.key_id
 }
--- rds/security-group.tf
+++ rds/security-group.tf
@@ -1,20 +1,20 @@
 resource "aws_security_group" "db" {
-  name = "${var.project}-${var.environment}-db"
+  name   = "${var.project}-${var.environment}-db"
   vpc_id = var.vpc_id
 
   tags = {
-    Name = "${var.project}-${var.environment}-db"
-    Project = var.project
+    Name        = "${var.project}-${var.environment}-db"
+    Project     = var.project
     Environment = var.environment
   }
 }
 
 resource "aws_security_group_rule" "db-from-cidr-blocks" {
-  count = length(var.allow_from_cidr_blocks)
-  type = "ingress"
-  from_port = 5432
-  to_port = 5432
-  protocol = "tcp"
+  count             = length(var.allow_from_cidr_blocks)
+  type              = "ingress"
+  from_port         = 5432
+  to_port           = 5432
+  protocol          = "tcp"
   security_group_id = aws_security_group.db.id
   cidr_blocks = [
     var.allow_from_cidr_blocks[count.index],
@@ -23,12 +23,12 @@ resource "aws_security_group_rule" "db-from-cidr-blocks" {
 }
 
 resource "aws_security_group_rule" "db-from-security-groups" {
-  count = length(var.allow_from_security_groups)
-  type = "ingress"
-  from_port = 5432
-  to_port = 5432
-  protocol = "tcp"
-  security_group_id = aws_security_group.db.id
+  count                    = length(var.allow_from_security_groups)
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.db.id
   source_security_group_id = var.allow_from_security_groups[count.index]
-  description = "From security group: ${var.allow_from_security_groups[count.index]}"
+  description              = "From security group: ${var.allow_from_security_groups[count.index]}"
 }
