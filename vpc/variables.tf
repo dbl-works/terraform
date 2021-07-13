73bcc822f5886544cb94b4f3d7b4d528861d9fde
swiknaba <swiknaba@users.noreply.github.com>
2021-07-13 16:12:58 +0200

    revert VPC cleanup

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -4,4 +4,3 @@ variable "project" {}
 variable "account_id" {}
 
 variable "region" { default = "eu-central-1" }
-variable "cidr_block" { default = "10.0.0.0/16" }
--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,3 +1,5 @@
+variable "cidr_block" { default = "10.0.0.0/16" }
+
 resource "aws_vpc" "vpc" {
   cidr_block = var.cidr_block
   enable_dns_hostnames = true
