3d8a8223d0a2d80c6498a827df3f3215921e565e
swiknaba <swiknaba@users.noreply.github.com>
2021-07-13 15:38:17 +0200

    print only first nlb dns name

--- ecs/outputs.tf
+++ ecs/outputs.tf
@@ -3,7 +3,7 @@ output "alb_dns_name" {
   value = aws_alb.alb.dns_name
 }
 output "nlb_dns_name" {
-  value = length(aws_lb.nlb) > 0 ? aws_lb.nlb[*].dns_name : null
+  value = length(aws_lb.nlb) > 0 ? aws_lb.nlb[0].dns_name : null
 }
 
 # Target groups, for use in deployments
