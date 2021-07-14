b89d2c80e387e5f77f65b0e3f8cbefb3782a618c
Marc Qualie <marc@marcqualie.com>
2021-07-14 18:18:01 +0100

    Add policy for listing + describing per project rds instances

--- rds/iam.tf
+++ rds/iam.tf
@@ -5,7 +5,7 @@ locals {
   ]
 }
 
-# Readonly access to system
+# Readonly access to database
 resource "aws_iam_group" "rds-db" {
   for_each = toset(local.db_roles)
   name     = "rds-db-${var.project}-${var.environment}-${each.key}"
@@ -37,4 +37,46 @@ resource "aws_iam_group_policy_attachment" "rds-db-connect" {
   for_each   = toset(local.db_roles)
   group      = aws_iam_group.rds-db[each.key].name
   policy_arn = aws_iam_policy.rds-db-connect[each.key].arn
+}
+
+
+
+# Grant access to list and describe instances
+resource "aws_iam_group" "rds" {
+  name     = "rds-${var.project}-${var.environment}"
+}
+resource "aws_iam_policy" "rds" {
+  name        = "rds_${var.project}_${var.environment}"
+  path        = "/"
+  description = "Allow viewing db instances for ${var.project} ${var.environment}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "rds:Describe*",
+        "rds:Get*",
+        "rds:List*"
+      ],
+      "Condition": {
+        "StringEquals": {
+          "aws:ResourceTag/Project": "${var.project}",
+          "aws:ResourceTag/Environment": "${var.environment}"
+        }
+      },
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_group_policy_attachment" "rds" {
+  for_each   = toset(local.db_roles)
+  group      = aws_iam_group.rds[each.key].name
+  policy_arn = aws_iam_policy.rds[each.key].arn
 }
