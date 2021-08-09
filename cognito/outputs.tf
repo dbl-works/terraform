2a80cccaf2ded82988713757028090cf00786631
Marc Qualie <marc@marcqualie.com>
2021-08-09 01:29:59 +0100

    Use .ts for template file extension

--- cognito/outputs.tf
+++ cognito/outputs.tf
@@ -12,7 +12,7 @@ output "aws_exports_content" {
     aws_cognito_identity_pool.main,
     aws_cognito_user_pool_client.client
   ]
-  value = templatefile("${path.module}/aws-exports.js.tmpl", {
+  value = templatefile("${path.module}/aws-exports.ts.tmpl", {
     region : var.region,
     user_pool_id : aws_cognito_user_pool.pool.id,
     identity_pool_id : aws_cognito_identity_pool.main.id,
