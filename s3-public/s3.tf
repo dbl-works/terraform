7c8f40c5d92134adb695b744e6e2dd09677cb6d1
Marc Qualie <marc@marcqualie.com>
2021-12-15 17:07:07 +0000

    aws api is still validating this, so use ternary

--- s3-public/s3.tf
+++ s3-public/s3.tf
@@ -8,10 +8,10 @@ resource "aws_s3_bucket" "main" {
 
   # Move data to a cheaper storage class after a period of time
   lifecycle_rule {
-    enabled = var.primary_storage_class_retention == -1 ? false : false
+    enabled = var.primary_storage_class_retention == 0 ? false : false
 
     noncurrent_version_transition {
-      days          = var.primary_storage_class_retention
+      days          = var.primary_storage_class_retention == 0 ? 365 : var.primary_storage_class_retention
       storage_class = "STANDARD_IA"
     }
   }
