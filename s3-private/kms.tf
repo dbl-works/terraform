40b8c7c064ee9f9d4210490e8f946b21e8f57b59
swiknaba <swiknaba@users.noreply.github.com>
2021-12-15 19:08:48 +0100

    better description for kms key

--- s3-private/kms.tf
+++ s3-private/kms.tf
@@ -4,6 +4,6 @@ module "kms-key-s3" {
   alias                   = "s3-${var.bucket_name}"
   project                 = var.project
   environment             = var.environment
-  description             = "Used for encrypting files in the storage bucket"
+  description             = "Used for encrypting files in a private bucket"
   deletion_window_in_days = var.kms_deletion_window_in_days
 }
