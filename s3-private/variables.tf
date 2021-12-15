047c85d65bb3cfd62189ae10c4b678e1e4367af4
swiknaba <swiknaba@users.noreply.github.com>
2021-12-15 16:22:28 +0100

    fix var name

--- s3-private/variables.tf
+++ s3-private/variables.tf
@@ -2,7 +2,7 @@ variable "project" {}
 variable "environment" {}
 variable "domain_name" {}
 variable "kms_deletion_window_in_days" { default = 30 }
-variable "bucket_name" {
+variable "bucket_name_prefix" {
   default = "storage"
 }
 
