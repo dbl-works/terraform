823f5bfdf18fa98805c19409c03d29f81d6adf20
swiknaba <swiknaba@users.noreply.github.com>
2021-12-15 15:47:01 +0100

    our apps aren't build for glacier

--- s3-private/s3.tf
+++ s3-private/s3.tf
@@ -13,11 +13,6 @@ resource "aws_s3_bucket" "main" {
       days          = 120
       storage_class = "STANDARD_IA"
     }
-
-    noncurrent_version_transition {
-      days          = 180
-      storage_class = "GLACIER"
-    }
   }
   server_side_encryption_configuration {
     rule {
