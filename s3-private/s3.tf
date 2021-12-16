501267f3c9877f300e12f7713e5d4bf0cb699586
Lud <swiknaba@users.noreply.github.com>
2021-12-17 00:36:27 +0100

    Adds Module for Standardized Storage Bucket (#51)
    * adds encrypted, private S3 bucket "storage"
    
    * update changelog
    
    * Update README.md
    
    * rename storage to private (module)
    
    * key name based on bucket name
    
    * better name for key
    
    * our apps aren't build for glacier
    
    * fix name tag
    
    * can't use vars inside vars
    
    * fix var name
    
    * allow people going crazy with the bucket name
    
    * better description for kms key
    
    * allow passing versioning and storage retention
    
    * update readmes
    
    * Update CHANGELOG.md
    
    Co-authored-by: Marc Qualie <marc@marcqualie.com>
    
    * by default version private files
    
    * adds type to kms deltion window variable
    
    * adds a descripton to kms deletion window var
    
    Co-authored-by: Marc Qualie <marc@marcqualie.com>

---
+++ s3-private/s3.tf
@@ -0,0 +1,31 @@
+resource "aws_s3_bucket" "main" {
+  bucket = var.bucket_name
+  acl    = "private"
+
+  versioning {
+    enabled = var.versioning
+  }
+
+  lifecycle_rule {
+    enabled = var.primary_storage_class_retention == 0 ? false : false
+
+    noncurrent_version_transition {
+      days          = var.primary_storage_class_retention == 0 ? 365 : var.primary_storage_class_retention
+      storage_class = "STANDARD_IA"
+    }
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = module.kms-key-s3.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  tags = {
+    Name        = var.bucket_name
+    Project     = var.project
+    Environment = var.environment
+  }
+}
