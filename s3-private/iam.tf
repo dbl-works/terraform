0720a7f97afd0510a2b7db4049eddb4a57c1bab6
Marc Qualie <marc@marcqualie.com>
2022-01-09 14:51:21 +0000

    Add object version actions

--- s3-private/iam.tf
+++ s3-private/iam.tf
@@ -23,9 +23,15 @@ resource "aws_iam_policy" "usage" {
       "Action": [
         "s3:DeleteObject",
         "s3:GetObject",
+        "s3:GetObjectVersion",
         "s3:ListBucket",
         "s3:PutObject",
-        "s3:PutObjectAcl"
+        "s3:PutObjectAcl",
+        "s3:PutObject",
+        "s3:PutObjectVersion",
+        "s3:PutObjectVersionAcl",
+        "s3:DeleteObject",
+        "s3:DeleteObjectVersion"
       ],
       "Resource": [
         "arn:aws:s3:::${var.bucket_name}",
