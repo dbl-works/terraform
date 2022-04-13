3f7bcb44a7693df0f44c44470e85954fc920cdf2
Sam Kah Chiin <samkahchiin@gmail.com>
2022-04-13 18:34:10 +0800

    [WIP] Replace deprecated syntax for s3-public

--- s3-private/s3.tf
+++ s3-private/s3.tf
@@ -35,7 +35,7 @@ resource "aws_s3_bucket_cors_configuration" "main-bucket-cors-configuration" {
 resource "aws_s3_bucket_versioning" "main-bucket-versioning" {
   bucket = aws_s3_bucket.main.id
   versioning_configuration {
-    status = var.versioning
+    status = var.versioning ? "Enabled" : "Disabled"
   }
 }
 
