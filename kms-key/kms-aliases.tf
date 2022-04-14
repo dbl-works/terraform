2ef595679e745c53bde97197acff2ae43821290d
Sam Kah Chiin <samkahchiin@gmail.com>
2022-04-14 20:24:32 +0800

    [WIP] Sanitize the string using replace function

--- kms-key/kms-aliases.tf
+++ kms-key/kms-aliases.tf
@@ -1,4 +1,4 @@
 resource "aws_kms_alias" "a" {
-  name          = "alias/${var.project}/${var.environment}/${var.alias}"
+  name          = replace("alias/${var.project}/${var.environment}/${var.alias}", "/[^a-zA-Z0-9:///_-]+/", "-")
   target_key_id = aws_kms_key.key.key_id
 }
