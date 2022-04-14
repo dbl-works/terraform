7aeeb9a5d61e64302d40a015472b511de50873e2
Sam Kah Chiin <samkahchiin@gmail.com>
2022-04-14 21:32:17 +0800

    [Chore] Replace S3 deprecated syntax (#68)
    * [WIP] Replace deprecated syntax for s3-private
    
    * [WIP] Replace deprecated syntax for s3-public
    
    * [WIP] Sanitize the string using replace function
    
    * [WIP] Add extensions recommendations
    
    * [WIP] Turn on formatOnSave for terraform file in the repo
    
    * [WIP] Update gitignore

--- s3-public/s3.tf
+++ s3-public/s3.tf
@@ -1,24 +1,34 @@
 resource "aws_s3_bucket" "main" {
   bucket = var.bucket_name
-  acl    = "public-read"
-
-  versioning {
-    enabled = var.versioning
-  }
-
-  # Move data to a cheaper storage class after a period of time
-  lifecycle_rule {
-    enabled = var.primary_storage_class_retention == 0 ? false : false
-
-    noncurrent_version_transition {
-      days          = var.primary_storage_class_retention == 0 ? 365 : var.primary_storage_class_retention
-      storage_class = "STANDARD_IA"
-    }
-  }
-
   tags = {
     Name        = var.bucket_name
     Project     = var.project
     Environment = var.environment
+  }
+}
+
+resource "aws_s3_bucket_versioning" "main-bucket-versioning" {
+  bucket = aws_s3_bucket.main.id
+  versioning_configuration {
+    status = var.versioning ? "Enabled" : "Disabled"
+  }
+}
+
+resource "aws_s3_bucket_acl" "main-bucket-data-acl" {
+  bucket = aws_s3_bucket.main.id
+  acl    = "public-read"
+}
+
+# Move data to a cheaper storage class after a period of time
+resource "aws_s3_bucket_lifecycle_configuration" "main-bucket-lifecycle-rule" {
+  bucket = aws_s3_bucket.main.id
+
+  rule {
+    id     = "primary-storage-class-retention"
+    status = var.primary_storage_class_retention == 0 ? "Disabled" : "Enabled"
+    noncurrent_version_transition {
+      noncurrent_days = var.primary_storage_class_retention == 0 ? 365 : var.primary_storage_class_retention
+      storage_class   = "STANDARD_IA"
+    }
   }
 }
