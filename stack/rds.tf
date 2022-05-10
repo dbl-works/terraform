b213c8576e38251ff83c6a548ab5b452f5d25a55
Sam Kah Chiin <samkahchiin@gmail.com>
2022-05-10 19:55:02 +0800

    [WIP] Update rds module

--- stack/rds.tf
+++ stack/rds.tf
@@ -2,12 +2,11 @@
 module "rds" {
   source = "github.com/dbl-works/terraform//rds?ref=${var.module_version}"
 
-  account_id = "12345678"
-  region     = "eu-central-1"
-  vpc_id     = "vpc-123"
-  project    = "someproject"
-  # TODO: Passing this as variables?
-  environment                = "staging"
+  account_id                 = var.account_id
+  region                     = var.region
+  vpc_id                     = "vpc-123"
+  project                    = var.project
+  environment                = var.environment
   password                   = "xxx"
   kms_key_arn                = "arn:aws:kms:eu-central-1:12345678:key/xxx-xxx"
   subnet_ids                 = ["subnet-1", "subnet-2"]
