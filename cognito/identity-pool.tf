71a1e209b6a0e1da818fed25c06a9dbe6dec9bcc
Sam Kah Chiin <samkahchiin@gmail.com>
2022-05-13 17:05:56 +0800

    [WIP] Add tags to identity pool

--- cognito/identity-pool.tf
+++ cognito/identity-pool.tf
@@ -2,4 +2,9 @@ resource "aws_cognito_identity_pool" "main" {
   identity_pool_name               = "${var.project}-${var.environment}"
   allow_unauthenticated_identities = false
   allow_classic_flow               = false
+
+  tags = {
+    Project     = var.project
+    Environment = var.environment
+  }
 }
