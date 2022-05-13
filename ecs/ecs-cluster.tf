0578942900b29a0ea4797d36b331a9507b023a36
Sam Kah Chiin <samkahchiin@gmail.com>
2022-05-13 23:55:25 +0800

    [WIP] Fix ecs-cluster fmt

--- ecs/ecs-cluster.tf
+++ ecs/ecs-cluster.tf
@@ -1,5 +1,5 @@
 resource "aws_ecs_cluster" "main" {
-  name               = "${var.project}-${var.environment}"
+  name = "${var.project}-${var.environment}"
 
   setting {
     name  = "containerInsights"
