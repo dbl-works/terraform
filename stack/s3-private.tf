0e43c2eaa4f7dd24bbbd6f50c4f25dc462bd7862
Sam Kah Chiin <samkahchiin@gmail.com>
2022-05-18 15:32:25 +0800

    [WIP] Use for_each instead of count

--- stack/s3-private.tf
+++ stack/s3-private.tf
@@ -1,15 +1,17 @@
 module "s3-storage" {
   source = "github.com/dbl-works/terraform//s3-private?ref=${var.module_version}"
 
-  count = length(var.private_buckets_list)
+  # This defines bucket_name as the key,
+  # thus allows terraform to track the resources by bucket_name (the key)
+  for_each = { for bucket in var.private_buckets_list : bucket.bucket_name => bucket }
 
   # Required
   environment = var.environment
   project     = var.project
-  bucket_name = var.private_buckets_list[count.index].bucket_name
+  bucket_name = each.value.bucket_name
 
   # Optional
-  kms_deletion_window_in_days     = var.private_buckets_list[count.index].kms_deletion_window_in_days
-  versioning                      = var.private_buckets_list[count.index].versioning
-  primary_storage_class_retention = var.private_buckets_list[count.index].primary_storage_class_retention
+  kms_deletion_window_in_days     = each.value.kms_deletion_window_in_days
+  versioning                      = each.value.versioning
+  primary_storage_class_retention = each.value.primary_storage_class_retention
 }
--- stack/s3-public.tf
+++ stack/s3-public.tf
@@ -1,14 +1,16 @@
 module "s3-frontend" {
   source = "github.com/dbl-works/terraform//s3-public?ref=${var.module_version}"
 
-  count = length(var.public_buckets_list)
+  # This defines bucket_name as the key,
+  # thus allows terraform to track the resources by bucket_name (the key)
+  for_each = { for bucket in var.public_buckets_list : bucket.bucket_name => bucket }
 
   # Required
   environment = var.environment
   project     = var.project
-  bucket_name = var.public_buckets_list[count.index].bucket_name
+  bucket_name = each.value.bucket_name
 
   # Optional
-  versioning                      = var.public_buckets_list[count.index].versioning
-  primary_storage_class_retention = var.public_buckets_list[count.index].primary_storage_class_retention
+  versioning                      = each.value.versioning
+  primary_storage_class_retention = each.value.primary_storage_class_retention
 }
