917e8c35638af6a33b4876006c9bd071ab8da5bb
Sam Kah Chiin <samkahchiin@gmail.com>
2022-05-18 19:41:29 +0800

    [WIP] Update ecs module

--- stack/ecs.tf
+++ stack/ecs.tf
@@ -5,26 +5,37 @@ module "ecs" {
   environment        = var.environment
   vpc_id             = module.vpc.id
   subnet_private_ids = module.vpc.subnet_private_ids
-  subnet_public_ids  = module.vpc.subnet_private_ids
-  secrets_arns       = []
-  kms_key_arns       = []
+  subnet_public_ids  = module.vpc.subnet_public_ids
+  secrets_arns = [
+    module.secrets["app"].arn
+  ]
+  kms_key_arns = flatten(concat([
+    module.kms-key.arn,
+    module.s3-storage.kms-key-arn
+  ]))
 
   # optional
-  health_check_path = "/healthz"
-  certificate_arn   = module.ssl-certificate.arn # requires a `certificate` module to be created separately
+  health_check_path = "/livez"
+  # requires a `certificate` module to be created separately
+  certificate_arn = module.certificate.arn
 
-  allow_internal_traffic_to_ports = []
+  allow_internal_traffic_to_ports = var.allow_internal_traffic_to_ports
 
-  allowlisted_ssh_ips = [
-    local.cidr_block,
-    "XX.XX.XX.XX/32", # e.g. a VPN
+  allowlisted_ssh_ips = var.allowlisted_ssh_ips
+
+  grant_read_access_to_s3_arns = []
+  grant_write_access_to_s3_arns = [
+    "${module.s3-storage.arn}/*",
   ]
-
-  grant_read_access_to_s3_arns  = []
-  grant_write_access_to_s3_arns = []
 
   grant_read_access_to_sqs_arns  = []
   grant_write_access_to_sqs_arns = []
 
-  custom_policies = []
+  custom_policies = var.ecs_custom_policies
+
+  depends_on = [
+    module.certificate.arn,
+    module.kms-key.arn,
+    module.s3-storage.kms-key-arn
+  ]
 }
