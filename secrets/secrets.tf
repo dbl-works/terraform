0253ccfb6452572abc98c06565760f0afb2eb6b7
Sam Kah Chiin <samkahchiin@gmail.com>
2022-05-19 13:04:18 +0800

    [WIP] Add kms_key_id to the secrets module

--- secrets/secrets.tf
+++ secrets/secrets.tf
@@ -3,6 +3,7 @@
 resource "aws_secretsmanager_secret" "main" {
   name        = "${var.project}/${var.application}/${var.environment}"
   description = try(var.description, "Secrets that are not to be stored inside ${var.application}.")
+  kms_key_id  = var.kms_key_id
 
   tags = {
     Project     = var.project
