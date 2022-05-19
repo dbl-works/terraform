5c62a68f9586508b5aea86e8e1e9d023fb933cb2
Sam Kah Chiin <samkahchiin@gmail.com>
2022-05-19 22:33:08 +0800

    [WIP] Update secrets manager

--- stack/secrets.tf
+++ stack/secrets.tf
@@ -11,13 +11,17 @@ locals {
   credentials = jsondecode(
     data.aws_secretsmanager_secret_version.terraform.secret_string
   )
+
+  app_secrets = {
+    "database_url": module.rds.database_url,
+    "redis_url": module.elasticache.endpoint
+  }
 }
 
 module "secrets" {
   source = "../secrets"
 
   for_each = local.secret_vaults
-  # NOTE: Not sure this will work
   kms_key_id = module.secrets-kms-key[each.key].id
 
   project     = var.project
@@ -28,8 +32,22 @@ module "secrets" {
   description = each.value.description
 }
 
+
+resource "aws_secretsmanager_secret_version" "app" {
+  secret_id     = module.secrets["app"].id
+  secret_string = jsonencode(merge(jsondecode(file("${path.cwd}/app-secrets.json")), local.app_secrets))
+}
+
+resource "aws_secretsmanager_secret_version" "terraform" {
+  secret_id = module.secrets["terraform"].id
+  secret_string = file("${path.cwd}/terraform-secrets.json")
+}
+
+# NOTE: Running this at the first time will throw Version "AWSCURRENT" not found because
+# aws_secretsmanager_secret_version is not created yet that time
+# The dependency should be resolve by terraform however it doesn't work here
 data "aws_secretsmanager_secret_version" "terraform" {
-  secret_id = "${var.project}/terraform/${var.environment}"
+  secret_id = module.secrets["terraform"].id
 }
 
 module "secrets-kms-key" {
