f64ce471ace07238bad2d289e451dfcf7faff99c
Sam Kah Chiin <samkahchiin@gmail.com>
2022-05-24 10:36:51 +0800

    [WIP] Add cloudflare initial setup

---
+++ cloudflare/variables.tf
@@ -0,0 +1,6 @@
+variable "cloudflare_email" {
+  type = string
+}
+variable "cloudflare_api_key" {
+  type = string
+}
---
+++ cloudflare/versions.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = ">= 3.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
