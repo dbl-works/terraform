da8036790db156c04f6189edd407aa3a77398bc1
Sam Kah Chiin <samkahchiin@gmail.com>
2022-05-27 16:50:25 +0800

    [WIP] Update taggable resources module

--- iam-policy-for-taggable-resources/project-admins-policies.tf
+++ iam-policy-for-taggable-resources/project-admins-policies.tf
@@ -1,6 +1,7 @@
-# Not sure whether user can manage Tag and iam role
+# TODO: Not sure whether user can manage Tag and iam role
 data "aws_iam_policy_document" "admin" {
   statement {
+    sid = "AllowAdminAccessBasedOnTags"
     actions = ["*"]
     resources = ["*"]
     condition {
@@ -10,17 +11,65 @@ data "aws_iam_policy_document" "admin" {
     }
 
     condition {
-      test     = "StringEquals"
-      variable = "aws:ResourceTag/Environment"
-      values = [var.environment]
+      test     = "StringLike"
+      variable = "aws:ResourceTag/Project"
+      values = ["&{aws:PrincipalTag/${var.environment}-admin-access-projects}"]
     }
   }
 }
 
-resource "aws_iam_policy" "admin-usage-for-taggable-resources" {
-  name        = "TaggableResourcesAdminUsage"
-  path        = "/"
-  description = "Allow admins to access all taggable resources in the project"
+data "aws_iam_policy_document" "developer" {
+  statement {
+    sid = "AllowDeveloperAccessBasedOnTags"
+    actions = [
+        "aws:Describe*",
+        "aws:Get*"
+    ]
+    resources = ["*"]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:ResourceTag/Environment"
+      values = [var.environment]
+    }
 
-  policy = data.aws_iam_policy_document.admin.json
+    condition {
+      test     = "StringLike"
+      variable = "aws:ResourceTag/Project"
+      values = ["&{aws:PrincipalTag/${var.environment}-developer-access-projects}"]
+    }
+  }
+}
+
+# TODO: Would this be too generous to allow the user to see the following list?
+# TODO: Move this to non-taggable resources
+# List action generally does not support resource-level permissions
+# https://stackoverflow.com/questions/58993574/iam-policy-with-awsresourcetag-not-supported
+data "aws_iam_policy_document" "list" {
+  statement {
+    sid = "AllowListAccessToAllResources"
+    actions = [
+        "secretsmanager:List*",
+        "kms:List*",
+        "s3:List*",
+        "ecr:List*",
+        "ecs:List*",
+    ]
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "taggable-resources" {
+  source_policy_documents = [
+    data.aws_iam_policy_document.admin.json,
+    data.aws_iam_policy_document.developer.json,
+    data.aws_iam_policy_document.list.json
+  ]
+}
+
+resource "aws_iam_policy" "taggable-resources" {
+  name        = "TaggableResourcesAccessOn${title(var.environment)}"
+  path        = "/"
+  description = "Allow access to taggable resources based on project on ${var.environment}"
+
+  policy = data.aws_iam_policy_document.taggable-resources.json
 }
