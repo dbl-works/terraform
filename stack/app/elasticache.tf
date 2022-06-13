8c2d8d99edccf782b33390e48e500f1e4bb5e343
Lud <swiknaba@users.noreply.github.com>
2022-06-13 21:31:11 +0200

    fix passing availability zones to elasticache

--- stack/app/elasticache.tf
+++ stack/app/elasticache.tf
@@ -1,11 +1,12 @@
 module "elasticache" {
   source = "../../elasticache"
 
-  project     = var.project
-  environment = var.environment
-  vpc_id      = module.vpc.id
-  subnet_ids  = module.vpc.subnet_private_ids
-  vpc_cidr    = module.vpc.cidr_block
+  project            = var.project
+  environment        = var.environment
+  vpc_id             = module.vpc.id
+  subnet_ids         = module.vpc.subnet_private_ids
+  vpc_cidr           = module.vpc.cidr_block
+  availability_zones = var.vpc_availability_zones
 
   # optional
   node_count = var.elasticache_node_count
