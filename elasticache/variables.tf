f390a701f096f6db8b1957388d6e4a9988485078
Lud <swiknaba@users.noreply.github.com>
2022-06-13 20:16:29 +0200

    All new features in one test branch (#82)
    https://github.com/dbl-works/terraform/pull/77

--- elasticache/variables.tf
+++ elasticache/variables.tf
@@ -2,6 +2,7 @@ variable "project" {}
 variable "environment" {}
 variable "vpc_id" {}
 variable "vpc_cidr" {}
+variable "kms_key_arn" {}
 variable "subnet_ids" { type = list(string) }
 variable "availability_zones" { type = list(string) }
 
@@ -20,4 +21,19 @@ variable "parameter_group_name" {
 variable "engine_version" {
   type    = string
   default = "6.x"
+}
+
+variable "snapshot_retention_limit" {
+  type    = number
+  default = 0
+}
+
+variable "replicas_per_node_group" {
+  default     = ""
+  description = "Replicas per Shard."
+}
+
+variable "num_node_groups" {
+  default     = ""
+  description = "Number of Shards (nodes)."
 }
