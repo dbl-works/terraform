205e49d4172d830071cd11859a8a2e078ec2f04f
Lud <swiknaba@users.noreply.github.com>
2022-06-16 17:50:29 +0800

    fix vpc cidr block for elasticache

--- stack/app/elasticache.tf
+++ stack/app/elasticache.tf
@@ -6,13 +6,11 @@ module "elasticache" {
   environment        = var.environment
   vpc_id             = module.vpc.id
   subnet_ids         = module.vpc.subnet_private_ids
-  vpc_cidr           = module.vpc.cidr_block
+  vpc_cidr           = var.vpc_cidr_block
   availability_zones = var.vpc_availability_zones
   kms_key_arn        = module.ecs-kms-key.arn
 
   # optional
   node_count = var.elasticache_node_count
   node_type  = var.elasticache_node_type
-
-  depends_on = [module.vpc]
 }
