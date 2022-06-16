04121ef49cac744c5b7ca6c9372ec36b8c443545
Lud <swiknaba@users.noreply.github.com>
2022-06-16 17:56:29 +0800

    fix: more merge errors

--- secrets/variables.tf
+++ secrets/variables.tf
@@ -1,3 +1,11 @@
 variable "project" {}
 variable "environment" {}
 variable "application" { default = "app" }
+
+variable "kms_key_id" {
+  type = string
+}
+
+variable "description" {
+  type = string
+}
