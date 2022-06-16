623e48906da979279d7cf81addf58ef9c9faac2d
Lud <swiknaba@users.noreply.github.com>
2022-06-16 18:02:11 +0800

    output certificate validation info

--- stack/app/outputs.tf
+++ stack/app/outputs.tf
@@ -5,3 +5,9 @@ output "database_url" {
 output "redis_url" {
   value = module.elasticache.endpoint
 }
+
+# pass this to the cloudflare module
+output "domain_validation_information" {
+  value       = module.certificate.domain_validation_information
+  description = "Used to complete certificate validation, e.g. in Cloudflare."
+}
