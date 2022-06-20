570a7c42571267616befb34fe70f70b2c231d46a
Lud <swiknaba@users.noreply.github.com>
2022-06-20 14:54:30 +0800

    fix: restore certificate changes accidentally deleted

--- certificate/variables.tf
+++ certificate/variables.tf
@@ -2,6 +2,10 @@ variable "environment" {}
 variable "project" {}
 variable "domain_name" {}
 
+variable "alternative_domains" {
+  default = []
+  type    = list(string)
+}
 variable "add_wildcard_subdomains" {
   type    = bool
   default = true
