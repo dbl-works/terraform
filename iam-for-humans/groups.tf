e7763fc77b196b1c7022d2aaa58c4be526cd148b
Lud <swiknaba@users.noreply.github.com>
2022-06-21 11:51:47 +0800

    remove unused engineer ref from iam-for-humans

--- iam-for-humans/groups.tf
+++ iam-for-humans/groups.tf
@@ -1,12 +1,4 @@
-locals {
-  group_names = [
-    "engineer",
-    "humans",
-  ]
-}
-
 resource "aws_iam_group" "main" {
-  for_each = toset(local.group_names)
-  name     = each.value
-  path     = "/"
+  name = "humans"
+  path = "/"
 }
