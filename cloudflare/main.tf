daec6e80600739975d9cade5a676022e81a2801a
Lud <swiknaba@users.noreply.github.com>
2022-06-23 12:12:53 +0800

    fix previous commit

--- cloudflare/main.tf
+++ cloudflare/main.tf
@@ -4,7 +4,7 @@ data "cloudflare_zone" "default" {
 
 # api.my-project.com to the NLB
 resource "cloudflare_record" "api" {
-  zone_id = cloudflare_zone.default.id
+  zone_id = data.cloudflare_zone.default.id
   name    = "api"
   type    = "CNAME"
   value   = var.nlb_dns_name
@@ -15,7 +15,7 @@ resource "cloudflare_record" "api" {
 resource "cloudflare_record" "bastion" {
   count = var.bastion_public_dns == null ? 0 : 1
 
-  zone_id = cloudflare_zone.default.id
+  zone_id = data.cloudflare_zone.default.id
   name    = "bastion"
   type    = "CNAME"
   value   = var.bastion_public_dns
@@ -27,7 +27,7 @@ resource "cloudflare_record" "bastion" {
 resource "cloudflare_record" "s3" {
   for_each = toset(["cdn", "app"])
 
-  zone_id = cloudflare_zone.default.id
+  zone_id = data.cloudflare_zone.default.id
   name    = each.key
   type    = "CNAME"
   value   = var.domain
@@ -35,13 +35,13 @@ resource "cloudflare_record" "s3" {
 }
 
 resource "cloudflare_worker_route" "cdn_route" {
-  zone_id     = cloudflare_zone.default.id
+  zone_id     = data.cloudflare_zone.default.id
   pattern     = "*cdn.${var.domain}/*"
   script_name = var.cdn_worker_script_name
 }
 
 resource "cloudflare_worker_route" "app_route" {
-  zone_id     = cloudflare_zone.default.id
+  zone_id     = data.cloudflare_zone.default.id
   pattern     = "*app.${var.domain}/*"
   script_name = var.app_worker_script_name
 }
