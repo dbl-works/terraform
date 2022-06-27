3148a5e072456f5e995c6aacd792cfb623a45efd
Sam Kah Chiin <samkahchiin@gmail.com>
2022-06-27 21:57:46 +0800

    [Chore] Add AWS and cloudflare setup to stack/app module (#94)
    * [WIP] Add cloudflare setup
    
    * [WIP] Add aws env

--- stack/app/versions.tf
+++ stack/app/versions.tf
@@ -1,5 +1,10 @@
 terraform {
   required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = ">= 3.0"
+    }
+
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.0"
