46304a5c05ffb272b900cf91005e6364462d0729
Lud <swiknaba@users.noreply.github.com>
2022-07-01 02:28:00 +0800

    require aws config alias for stack

--- stack/app/versions.tf
+++ stack/app/versions.tf
@@ -6,8 +6,9 @@ terraform {
     }
 
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0"
+      source                = "hashicorp/aws"
+      version               = ">= 4.0"
+      configuration_aliases = [aws.peer]
     }
   }
   required_version = ">= 1.0"
