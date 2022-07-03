1615186f3caf9f511a9957958807ed877c529fd9
Lud <swiknaba@users.noreply.github.com>
2022-07-03 19:24:40 +0800

    fix copy pasta

--- kms-key-replica/versions.tf
+++ kms-key-replica/versions.tf
@@ -1,9 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      version               = ">= 4.0"
-      configuration_aliases = [aws.peer]
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
     }
   }
   required_version = ">= 1.0"
