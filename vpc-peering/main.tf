23db5d70693887f57dcfac1504391af965da2058
Lud <swiknaba@users.noreply.github.com>
2022-07-03 19:30:15 +0800

    fix: re-add accidentally deleted code

--- vpc-peering/main.tf
+++ vpc-peering/main.tf
@@ -17,3 +17,17 @@ resource "aws_vpc_peering_connection" "peer" {
     Environment = var.environment
   }
 }
+
+# Accepter's side of the connection.
+resource "aws_vpc_peering_connection_accepter" "peer" {
+  provider                  = aws.peer
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  auto_accept               = true
+
+  tags = {
+    Side        = "Accepter"
+    Name        = "${var.project}-${var.environment}-${var.accepter_region}-${var.requester_region}"
+    Project     = var.project
+    Environment = var.environment
+  }
+}
