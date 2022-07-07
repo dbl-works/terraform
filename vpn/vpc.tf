706c1342f8527c582732d8cd0ccd7211b69d23b8
Sam Kah Chiin <samkahchiin@gmail.com>
2022-07-07 13:33:00 +0800

    [Hotfix] AZ should be an array

--- vpn/vpc.tf
+++ vpn/vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source = "../vpc"
 
   account_id         = var.account_id
-  availability_zones = local.availability_zone
+  availability_zones = [local.availability_zone]
   environment        = var.environment
   project            = var.project
   region             = var.region
