a689a12c8d67d60b9982a7b77b41d909b39d10b2
Lud <swiknaba@users.noreply.github.com>
2022-08-01 23:55:21 +0200

    accepter route table must be in accepter region

--- vpc-peering/route-tables.tf
+++ vpc-peering/route-tables.tf
@@ -12,10 +12,12 @@ resource "aws_route" "requester_route" {
 
 # Accepter's side of the connection.
 resource "aws_route_table" "accepter_table" {
-  vpc_id = var.accepter_vpc_id
+  provider = aws.peer
+  vpc_id   = var.accepter_vpc_id
 }
 
 resource "aws_route" "accepter_route" {
+  provider                  = aws.peer
   route_table_id            = aws_route_table.accepter_table.id
   destination_cidr_block    = var.requester_cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
