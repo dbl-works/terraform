f42ebc93381b26ee438439c517c706b088f6db1d
Lud <swiknaba@users.noreply.github.com>
2022-08-01 20:26:43 +0200

    rds: also pass security groups through

--- stack/app/rds.tf
+++ stack/app/rds.tf
@@ -16,15 +16,19 @@ module "rds-kms-key" {
 module "rds" {
   source = "../../rds"
 
-  project                    = var.project
-  environment                = var.environment
-  account_id                 = var.account_id
-  region                     = var.region
-  vpc_id                     = module.vpc.id
-  password                   = var.rds_is_read_replica ? null : local.credentials.db_root_password
-  kms_key_arn                = var.rds_master_db_kms_key_arn == null ? module.rds-kms-key[0].arn : var.rds_master_db_kms_key_arn
-  subnet_ids                 = module.vpc.subnet_private_ids
-  allow_from_security_groups = [module.ecs.ecs_security_group_id]
+  project     = var.project
+  environment = var.environment
+  account_id  = var.account_id
+  region      = var.region
+  vpc_id      = module.vpc.id
+  password    = var.rds_is_read_replica ? null : local.credentials.db_root_password
+  kms_key_arn = var.rds_master_db_kms_key_arn == null ? module.rds-kms-key[0].arn : var.rds_master_db_kms_key_arn
+  subnet_ids  = module.vpc.subnet_private_ids
+
+  allow_from_security_groups = distinct(flatten(concat([
+    module.ecs.ecs_security_group_id,
+    var.rds_allow_from_security_groups
+  ])))
 
   # optional
   username               = var.rds_is_read_replica ? null : local.credentials.db_username
--- stack/app/variables.tf
+++ stack/app/variables.tf
@@ -172,6 +172,10 @@ variable "rds_allow_from_cidr_blocks" {
   type    = list(string)
   default = []
 }
+variable "rds_allow_from_security_groups" {
+  type    = list(string)
+  default = []
+}
 # =============== RDS ================ #
 
 # =============== ECS ================ #
