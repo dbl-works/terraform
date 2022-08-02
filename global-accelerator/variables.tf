a6420a18600788d2db649865f93f55cc4e3a87cb
Lud <swiknaba@users.noreply.github.com>
2022-08-02 21:18:09 +0200

    fix copy paste

--- global-accelerator/variables.tf
+++ global-accelerator/variables.tf
@@ -6,6 +6,16 @@ variable "environment" {
   type = string
 }
 
+variable "load_balancers" {
+  type = list(object({
+    region   = string
+    endpoint = string
+  }))
+}
+
+#
+# optional variables
+#
 variable "client_affinity" {
   type    = string
   default = "SOURCE_IP"
@@ -16,14 +26,7 @@ variable "health_check_path" {
   default = "/livez"
 }
 
-variable "client_affinity" {
+variable "health_check_port" {
   type    = number
   default = 3000
-}
-
-variable "load_balancers" {
-  type = list(object({
-    region   = string
-    endpoint = string
-  }))
 }
