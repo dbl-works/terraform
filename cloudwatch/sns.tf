c9c5fabcbe6f137986b63b3f37c213d594b01401
Sam Kah Chiin <samkahchiin@gmail.com>
2022-08-18 01:14:37 +0800

    [Finishes] Add sns and chatbot to the cloudwatch module

---
+++ cloudwatch/sns.tf
@@ -0,0 +1,45 @@
+resource "aws_sns_topic" "cloudwatch_slack" {
+  name = "${local.name}-cloudwatch-slack"
+}
+
+resource "aws_sns_topic_policy" "cloudwatch_slack" {
+  arn = aws_sns_topic.cloudwatch_slack.arn
+
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
+}
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  policy_id = "__default_policy_ID"
+
+  statement {
+    sid = "__default_statement_ID"
+    effect = "Allow"
+    actions = [
+      "SNS:Subscribe",
+      "SNS:SetTopicAttributes",
+      "SNS:RemovePermission",
+      "SNS:Receive",
+      "SNS:Publish",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:DeleteTopic",
+      "SNS:AddPermission",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        var.account_id,
+      ]
+    }
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    resources = [aws_sns_topic.cloudwatch_slack.arn]
+  }
+}
