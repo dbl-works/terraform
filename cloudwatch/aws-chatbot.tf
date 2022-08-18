66a2261992f3fedb25398c88b91c65b4fe9de0c2
Sam Kah Chiin <samkahchiin@gmail.com>
2022-08-18 10:53:22 +0800

    [WIP] Disable chatbot

--- cloudwatch/aws-chatbot.tf
+++ cloudwatch/aws-chatbot.tf
@@ -1,16 +1,16 @@
-resource "awscc_chatbot_slack_channel_configuration" "chatbot" {
-  configuration_name = "${local.name}-chatbot"
-  iam_role_arn       = aws_iam_role.cloudwatch_access_for_chatbot.arn
-  slack_channel_id   = var.slack_channel_id
-  slack_workspace_id = var.slack_workspace_id
-  user_role_required = false
-  sns_topic_arns     = [aws_sns_topic.cloudwatch_slack.arn]
-  guardrail_policies = []
-}
+# resource "awscc_chatbot_slack_channel_configuration" "chatbot" {
+#   configuration_name = "${local.name}-chatbot"
+#   iam_role_arn       = aws_iam_role.cloudwatch_access_for_chatbot.arn
+#   slack_channel_id   = var.slack_channel_id
+#   slack_workspace_id = var.slack_workspace_id
+#   user_role_required = false
+#   sns_topic_arns     = [aws_sns_topic.cloudwatch_slack.arn]
+#   guardrail_policies = []
+# }
 
-resource "aws_iam_role" "cloudwatch_access_for_chatbot" {
-  name = "cloudwatch_access_for_chatbot"
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess",
-  ]
-}
+# resource "aws_iam_role" "cloudwatch_access_for_chatbot" {
+#   name = "cloudwatch_access_for_chatbot"
+#   managed_policy_arns = [
+#     "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess",
+#   ]
+# }
