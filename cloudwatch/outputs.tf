7fb9e016d619ac30279c477d4ef4ab30994a69c4
Sam Kah Chiin <samkahchiin@gmail.com>
2022-08-20 19:07:36 +0800

    [Hotfix] Update sns_topic_arn in cloudwatch output

--- cloudwatch/outputs.tf
+++ cloudwatch/outputs.tf
@@ -1,3 +1,3 @@
 output "sns_topic_arn" {
-  value = length(module.chatbot) > 0 ? module.chatbot[0].sns_topic_arn : null
+  value = length(module.chatbot) > 0 ? module.chatbot[0].sns_topic_arn : var.sns_topic_arn
 }
