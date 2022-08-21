466d44604675909820f484f7b65f30068fda083d
Sam Kah Chiin <samkahchiin@gmail.com>
2022-08-21 20:13:49 +0800

    [Finishes] Remove slack and sns from cloudwatch

--- cloudwatch/chatbot.tf
+++
@@ -1,9 +0,0 @@
-module "chatbot" {
-  count  = var.slack_channel_id != null && var.slack_workspace_id != null ? 1 : 0
-  source = "../slack-chatbot"
-
-  chatbot_name       = "${local.name}-chatbot"
-  slack_channel_id   = var.slack_channel_id
-  slack_workspace_id = var.slack_workspace_id
-  guardrail_policies = ["arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"]
-}
--- slack-chatbot/variables.tf
+++ slack-chatbot/variables.tf
@@ -9,14 +9,3 @@ variable "slack_channel_id" {
 variable "slack_workspace_id" {
   type = string
 }
-
-variable "sns_topic_name" {
-  type    = string
-  default = "slack-sns"
-}
-
-variable "guardrail_policies" {
-  type        = list(string)
-  default     = []
-  description = "Allowable permissions in the channel thru chatbot"
-}
