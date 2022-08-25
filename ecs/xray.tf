da914d924f53bd8321e3cde254fcb7296d37d62d
Sam Kah Chiin <samkahchiin@gmail.com>
2022-08-25 19:57:08 +0800

    [Hotfix] Quote the name in filter expression

--- ecs/xray.tf
+++ ecs/xray.tf
@@ -1,11 +1,9 @@
 resource "aws_xray_group" "ecs_filter" {
   count      = var.enable_xray ? 1 : 0
   group_name = local.name
+  # How to configure filter:https://docs.aws.amazon.com/xray/latest/devguide/xray-console-filters.html
   # Return traces for requests that hit the ecs on the service map
-  # Check filter here
-  # https://docs.aws.amazon.com/xray/latest/devguide/xray-console-filters.html
-  # '/livez', '/readyz', '/checks/ip', '/healthz', '/'
-  filter_expression = "service(${local.name}) AND !service('in.logtail.com')"
+  filter_expression = "service('${local.name}') AND !service('in.logtail.com')"
 
   insights_configuration {
     insights_enabled = true
