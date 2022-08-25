bd6f6091778cc8ce48fd3f934f2f55e9823f3d8f
Sam Kah Chiin <samkahchiin@gmail.com>
2022-08-25 20:17:20 +0800

    [Finishes] Add error group

--- ecs/xray.tf
+++ ecs/xray.tf
@@ -3,11 +3,33 @@ resource "aws_xray_group" "ecs_filter" {
   group_name = local.name
   # How to configure filter:https://docs.aws.amazon.com/xray/latest/devguide/xray-console-filters.html
   # Return traces for requests that hit the ecs on the service map
-  filter_expression = "service(\"${local.name}\") AND !service(\"in.logtail.com\")"
+  filter_expression = "service(\"${local.name}\")"
 
   insights_configuration {
     insights_enabled = true
     # TODO: Setup event bridge and trigger slack
+    notifications_enabled = true
+  }
+}
+
+resource "aws_xray_group" "ecs_server_error" {
+  count             = var.enable_xray ? 1 : 0
+  group_name        = "${local.name}-server-error"
+  filter_expression = "service(\"${local.name}\") { error }"
+
+  insights_configuration {
+    insights_enabled      = true
+    notifications_enabled = true
+  }
+}
+
+resource "aws_xray_group" "ecs_client_error" {
+  count             = var.enable_xray ? 1 : 0
+  group_name        = "${local.name}-client-error"
+  filter_expression = "service(\"${local.name}\") { fault }"
+
+  insights_configuration {
+    insights_enabled      = true
     notifications_enabled = true
   }
 }
