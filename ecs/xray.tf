605a59aa395dc1021f47799af25ddad2e45f51f0
Sam Kah Chiin <samkahchiin@gmail.com>
2022-08-25 16:06:57 +0800

    [WIP]

--- ecs/xray.tf
+++ ecs/xray.tf
@@ -7,21 +7,41 @@ locals {
 
 }
 
-# resource "aws_xray_group" "ecs_filter" {
-#   count      = var.enable_xray ? 1 : 0
-#   group_name = local.name
-#   # Return traces for requests that hit the ecs on the service map
-#   # Check filter here
-#   # https://docs.aws.amazon.com/xray/latest/devguide/xray-console-filters.html
-#   # '/livez', '/readyz', '/checks/ip', '/healthz', '/'
-#   filter_expression = "service(${local.name}) ${local.health_check_expression}"
+resource "aws_xray_group" "ecs_filter" {
+  count      = var.enable_xray ? 1 : 0
+  group_name = local.name
+  # Return traces for requests that hit the ecs on the service map
+  # Check filter here
+  # https://docs.aws.amazon.com/xray/latest/devguide/xray-console-filters.html
+  # '/livez', '/readyz', '/checks/ip', '/healthz', '/'
+  filter_expression = "service(${local.name}) AND !service('in.logtail.com') ${local.health_check_expression}"
 
-#   insights_configuration {
-#     insights_enabled = true
-#     # TODO: Setup event bridge and trigger slack
-#     notifications_enabled = true
-#   }
-# }
+  insights_configuration {
+    insights_enabled = true
+    # TODO: Setup event bridge and trigger slack
+    notifications_enabled = true
+  }
+}
+
+resource "aws_xray_sampling_rule" "ecs" {
+  rule_name      = "filter health check"
+  priority       = 1000
+  version        = 1
+  reservoir_size = 1
+  # The percentage of matching requests to instrument, after the reservoir is exhausted. The rate can be an integer or a float.
+  fixed_rate     = 0.05
+  url_path       = "*"
+  host           = "*"
+  http_method    = "*"
+  service_type   = "AWS::ECS::Container"
+  service_name   = "*"
+  # X-Ray SDK – Not supported. The SDK can only use rules with Resource ARN set to *.
+  resource_arn   = "*"
+
+  attributes = {
+    Hello = "Tris"
+  }
+}
 
 # resource "aws_xray_group" "ecs_filter" {
 #   count      = var.enable_xray ? 1 : 0
