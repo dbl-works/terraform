56b53bc81f5c5f825f0b2bb15fc2ac271b673a19
Sam Kah Chiin <samkahchiin@gmail.com>
2022-08-25 15:54:07 +0800

    [Finishes] Add aws_xray_group

--- ecs/xray.tf
+++ ecs/xray.tf
@@ -1,14 +1,39 @@
-resource "aws_xray_group" "ecs" {
-  count      = var.enable_xray ? 1 : 0
-  group_name = local.name
-  # Return traces for requests that hit the ecs on the service map
-  # Check filter here
-  # https://docs.aws.amazon.com/xray/latest/devguide/xray-console-filters.html
-  filter_expression = "service(${local.name})"
+locals {
+  health_check_urls = [
+    "/livez", "/readyz", "/checks/ip", "/healthz"
+  ]
 
-  insights_configuration {
-    insights_enabled = true
-    # TODO: Setup event bridge and trigger slack
-    notifications_enabled = true
-  }
+ health_check_expression = join(" ", [for url in local.health_check_urls : "AND !(http.url CONTAINS '${url}')"])
+
 }
+
+# resource "aws_xray_group" "ecs_filter" {
+#   count      = var.enable_xray ? 1 : 0
+#   group_name = local.name
+#   # Return traces for requests that hit the ecs on the service map
+#   # Check filter here
+#   # https://docs.aws.amazon.com/xray/latest/devguide/xray-console-filters.html
+#   # '/livez', '/readyz', '/checks/ip', '/healthz', '/'
+#   filter_expression = "service(${local.name}) ${local.health_check_expression}"
+
+#   insights_configuration {
+#     insights_enabled = true
+#     # TODO: Setup event bridge and trigger slack
+#     notifications_enabled = true
+#   }
+# }
+
+# resource "aws_xray_group" "ecs_filter" {
+#   count      = var.enable_xray ? 1 : 0
+#   group_name = local.name
+#   # Return traces for requests that hit the ecs on the service map
+#   # Check filter here
+#   # https://docs.aws.amazon.com/xray/latest/devguide/xray-console-filters.html
+#   filter_expression = "service(${local.name})"
+
+#   insights_configuration {
+#     insights_enabled = true
+#     # TODO: Setup event bridge and trigger slack
+#     notifications_enabled = true
+#   }
+# }
