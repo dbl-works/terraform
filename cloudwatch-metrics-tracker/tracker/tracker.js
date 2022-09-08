3a4801b943e9088b6ccb91ad799c1c37ec0a9402
Sam Kah Chiin <samkahchiin@gmail.com>
2022-09-08 14:26:00 +0800

    [Refactor] Further refactor

--- cloudwatch-metrics-tracker/tracker/tracker.js
+++ cloudwatch-metrics-tracker/tracker/tracker.js
@@ -2,18 +2,8 @@ const AWS = require('aws-sdk')
 const cloudwatch = new AWS.CloudWatch()
 const dateUtil = require('./utils/date');
 const fivetran = require('./utils/fivetran');
+const cloud = require('./utils/cloudwatch');
 const constants = require('./constants');
-
-const ECS_METRICS = [
-  'CPUUtilization',
-  'MemoryUtilization'
-]
-
-const PERCENTILES = [
-  'p99',
-  'p90',
-  'p50'
-]
 
 function formatTransactionRows ({ dataPoints, params }) {
   return dataPoints.map(data => {
@@ -61,62 +51,6 @@ function setupErrorCountsQueries ({ projectName, loadBalancerName }) {
   }
 }
 
-function setupResponseTimesQueries ({ projectName, loadBalancerName }) {
-  return PERCENTILES.map(percentile => ({
-    // TODO: Have to be more specific
-    Id: `${percentile.toLowerCase()}_${projectName}`, // /^[a-z][a-zA-Z0-9_]*$./
-    MetricStat: {
-      Metric: {
-        Dimensions: [
-          {
-            Name: 'LoadBalancer',
-            Value: loadBalancerName
-          },
-        ],
-        MetricName: 'TargetResponseTime', /* required */
-        Namespace: 'AWS/ApplicationELB', /* required */
-      },
-      Period: constants.PERIOD,
-      Stat: percentile, /* required */
-      Unit: 'Seconds' // If we set it as Milliseconds, the value returned will be undefined
-    },
-    ReturnData: true
-  }))
-}
-
-function setupECSMetricQueries ({ serviceName, clusterName, projectName }) {
-  return ECS_METRICS.map(metric => ({
-    Id: `${metric.toLowerCase()}_${projectName}`, /* required */
-    MetricStat: {
-      Metric: {
-        Dimensions: [
-          {
-            Name: 'ServiceName',
-            Value: serviceName
-          },
-          {
-            Name: 'ClusterName',
-            Value: clusterName
-          }
-        ],
-        MetricName: metric, /* required */
-        Namespace: 'AWS/ECS', /* required */
-      },
-      Period: constants.PERIOD,
-      Stat: 'Maximum', /* required */
-      Unit: 'Percent'
-    },
-    ReturnData: true
-  }))
-}
-
-function setupPerformanceMetricQueries (data) {
-  return [
-    ...setupECSMetricQueries(data),
-    ...setupResponseTimesQueries(data)
-  ]
-}
-
 function setupMetricParams (metricDataQueries) {
   const prevHour = dateUtil.previousHour()
 
@@ -125,37 +59,6 @@ function setupMetricParams (metricDataQueries) {
     StartTime: dateUtil.oneHourBefore(prevHour),
     EndTime: prevHour
   }
-}
-
-// TODO: Think of how to make sure the lambda does not retrieve the same data twice
-// (last record endtime -> prevCursor) -> (new record endtime -> current Cursor)
-
-async function getPerformanceMetrics () {
-  const resourcesData = JSON.parse(constants.RESOURCES_DATA)
-  const metricDataQueries = resourcesData.flatMap((data) => setupPerformanceMetricQueries(data))
-  const params = setupMetricParams(metricDataQueries)
-  // NOTE: A single GetMetricData call can include as many as 500 MetricDataQuery structures.
-  // NOTE: We can't retrieve data from region different than the lambda region
-  const { MetricDataResults: ecsDataPoints } = await cloudwatch.getMetricData(params).promise()
-  // TODO: Have another call one for the sum of one day 500 error count, period = 1 day at the end of day
-  // TODO: one day based on eu central time?
-
-  // Sample Response
-  // {
-  //   ResponseMetadata: { RequestId: 'fe077633-9093-47e7-8e64-d26c244494bb' },
-  //   MetricDataResults: [
-  //     {
-  //       Id: 'cpu',
-  //       Label: 'CPUUtilization',
-  //       Timestamps: [ 2022-09-07T15:00:00.000Z ],
-  //       Values: [ 21.19099235534668 ],
-  //       StatusCode: 'Complete',
-  //       Messages: []
-  //     }
-  //   ],
-  //   Messages: []
-  // }
-  return formatTransactionRows({ dataPoints: ecsDataPoints, params })
 }
 
 async function getErrorCountMetrics () {
@@ -180,7 +83,7 @@ exports.handler = async (request, context, callback) => {
   try {
 
     // TODO: next if computed end time is after the prevCursor
-    const performanceTransactionRows = await getPerformanceMetrics()
+    const performanceTransactionRows = await cloud.getPerformanceMetrics()
     // TODO: Only run this at the midnight
     const errorCountTransactionRows = await getErrorCountMetrics()
     const newTransactions = [...performanceTransactionRows, ...errorCountTransactionRows]
---
+++ cloudwatch-metrics-tracker/tracker/utils/cloudwatch.js
@@ -0,0 +1,137 @@
+const AWS = require('aws-sdk')
+const constants = require('./../constants');
+const dateUtil = require('./date');
+
+const cloudwatch = new AWS.CloudWatch()
+
+const ECS_METRICS = [
+  'CPUUtilization',
+  'MemoryUtilization'
+]
+const PERCENTILES = [
+  'p99',
+  'p90',
+  'p50'
+]
+
+const ecsMetricQueries = ({ serviceName, clusterName, projectName }) => {
+  return ECS_METRICS.map(metric => ({
+    // NEed to be more specific
+    Id: `${metric.toLowerCase()}_${projectName}`, /* required */
+    MetricStat: {
+      Metric: {
+        Dimensions: [
+          {
+            Name: 'ServiceName',
+            Value: serviceName
+          },
+          {
+            Name: 'ClusterName',
+            Value: clusterName
+          }
+        ],
+        MetricName: metric, /* required */
+        Namespace: 'AWS/ECS', /* required */
+      },
+      Period: constants.PERIOD,
+      Stat: 'Maximum', /* required */
+      Unit: 'Percent'
+    },
+    ReturnData: true
+  }))
+}
+
+const responseTimesQueries = ({ projectName, loadBalancerName }) => {
+  return PERCENTILES.map(percentile => ({
+    // TODO: Have to be more specific
+    Id: `${percentile.toLowerCase()}_${projectName}`, // /^[a-z][a-zA-Z0-9_]*$./
+    MetricStat: {
+      Metric: {
+        Dimensions: [
+          {
+            Name: 'LoadBalancer',
+            Value: loadBalancerName
+          },
+        ],
+        MetricName: 'TargetResponseTime', /* required */
+        Namespace: 'AWS/ApplicationELB', /* required */
+      },
+      Period: constants.PERIOD,
+      Stat: percentile, /* required */
+      Unit: 'Seconds' // If we set it as Milliseconds, the value returned will be undefined
+    },
+    ReturnData: true
+  }))
+}
+
+//TODO: specific the arguments here
+const performanceMetricQueries = (data) => {
+  return [
+    ...ecsMetricQueries(data),
+    ...responseTimesQueries(data)
+  ]
+}
+
+// TODO: Think of how to make sure the lambda does not retrieve the same data twice
+// (last record endtime -> prevCursor) -> (new record endtime -> current Cursor)
+
+const formatTransactionRows = ({ dataPoints, params }) => {
+  return dataPoints.map(data => {
+    const queryParam = params.MetricDataQueries.find((param) =>
+      param.Id === data.Id
+    )
+
+    const [_metric, projectName] = queryParam.Id.split("_")
+    const unit = queryParam?.MetricStat.Unit
+
+    return {
+      project_name: projectName,
+      region: constants.AWS_REGION,
+      metric_name: queryParam?.MetricStat?.Metric?.MetricName,
+      dimensions: queryParam?.MetricStat?.Metric?.Dimensions,
+      start_time: params.StartTime.toISOString(),
+      end_time: params.EndTime.toISOString(),
+      stat: queryParam?.MetricStat?.Stat,
+      value: data.Values[0] || (unit === 'Count' ? 0 : "Null"),
+      unit,
+      created_at: new Date().toISOString()
+    }
+  })
+}
+const getPerformanceMetrics = async () => {
+  const resourcesData = JSON.parse(constants.RESOURCES_DATA)
+  const metricDataQueries = resourcesData.flatMap((data) => performanceMetricQueries(data))
+
+  const prevHour = dateUtil.previousHour()
+  const params = {
+    MetricDataQueries: metricDataQueries,
+    StartTime: dateUtil.oneHourBefore(prevHour),
+    EndTime: prevHour
+  }
+
+  // NOTE: A single GetMetricData call can include as many as 500 MetricDataQuery structures.
+  // NOTE: We can't retrieve data from region different than the lambda region
+  const { MetricDataResults: ecsDataPoints } = await cloudwatch.getMetricData(params).promise()
+  // Sample API Response
+  // {
+  //   ResponseMetadata: { RequestId: 'fe077633-9093-47e7-8e64-d26c244494bb' },
+  //   MetricDataResults: [
+  //     {
+  //       Id: 'cpu_utilization',
+  //       Label: 'CPUUtilization',
+  //       Timestamps: [ 2022-09-07T15:00:00.000Z ],
+  //       Values: [ 21.19099235534668 ],
+  //       StatusCode: 'Complete',
+  //       Messages: []
+  //     }
+  //   ],
+  //   Messages: []
+  // }
+  return formatTransactionRows({ dataPoints: ecsDataPoints, params })
+}
+
+
+
+module.exports = {
+  getPerformanceMetrics,
+}
