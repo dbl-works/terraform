3b92aa7e6383dd1e63e86c3e5170e948fe905143
Sam Kah Chiin <samkahchiin@gmail.com>
2022-09-30 18:38:14 +0800

    [WIP] Fix output

--- cloudwatch-snowflake/outputs.tf
+++ cloudwatch-snowflake/outputs.tf
@@ -1,3 +1,3 @@
 output "lambda_role_arn" {
-  value = var.lambda_role_arn == null ? aws_iam_role.lambda.arn : var.lambda_role_arn
+  value = var.lambda_role_arn == null ? aws_iam_role.lambda[*].arn : var.lambda_role_arn
 }
