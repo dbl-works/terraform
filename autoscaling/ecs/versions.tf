3bc28036b274bababd692ab515bc4fed050481c2
Sam Kah Chiin <samkahchiin@gmail.com>
2022-10-06 12:33:26 +0800

    [WIP] Remove expermental features

--- autoscaling/ecs/versions.tf
+++ autoscaling/ecs/versions.tf
@@ -2,11 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0"
+      version = "~> 4.0"
     }
   }
   # We need the optional attributes for this module
   # https://github.com/hashicorp/terraform/releases/tag/v1.3.0
-  required_version = ">= 1.3"
-  experiments      = [module_variable_optional_attrs]
+  required_version = "~> 1.3"
 }
