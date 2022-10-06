57f6377d5980c8a5c968db9fe8831492020a30de
Sam Kah Chiin <samkahchiin@gmail.com>
2022-10-06 13:51:16 +0800

    [WIP] Fix

--- autoscaling/ecs/alarms.tf
+++ autoscaling/ecs/alarms.tf
@@ -1,6 +1,9 @@
 locals {
-  threshold_up          = var.threshold_up
-  scale_up_expression   = join(" || ", [for metric in var.autoscale_metrics : "${lower(metric.metric_name)} > ${lookup(metric, "threshold_up", local.threshold_up)}"])
+  scale_up_expression = join(" || ", [
+    for metric in var.autoscale_metrics :
+    "${lower(metric.metric_name)} > ${lookup(metric, "threshold_up", "${var.threshold_up}")}"
+    ]
+  )
   scale_down_expression = join(" || ", [for metric in var.autoscale_metrics : "${lower(metric.metric_name)} < ${lookup(metric, "threshold_down", var.threshold_down)}"])
 }
 
