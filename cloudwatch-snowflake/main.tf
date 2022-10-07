517f5409ad7a132fe6f4ea701493e11ce5da0f1e
Sam Kah Chiin <samkahchiin@gmail.com>
2022-10-07 12:20:34 +0800

    [WIP] Revert cloudwatch snowflake changes

--- cloudwatch-snowflake/main.tf
+++ cloudwatch-snowflake/main.tf
@@ -1,26 +1,74 @@
-module "fivetran_lambda" {
-  source = "../fivetran/connectors/lambda"
+data "archive_file" "zip" {
+  type        = "zip"
+  source_dir  = var.lambda_source_dir == null ? "${path.module}/tracker" : var.lambda_source_dir
+  output_path = var.lambda_output_path == null ? "${path.module}/dist/tracker.zip" : var.lambda_output_path
+}
 
-  fivetran_api_key       = var.fivetran_api_key
-  fivetran_api_secret    = var.fivetran_api_secret
-  fivetran_group_id      = var.fivetran_group_id
-  organisation           = var.organisation
-  tracked_resources_data = []
+resource "aws_lambda_function" "cloudwatch_metrics_tracker" {
+  function_name = "cloudwatch_metrics_tracker_${var.organisation}_${var.aws_region_code}"
+  description   = "Collect AWS Cloudwatch Metrics"
+  role          = var.lambda_role_arn == null ? aws_iam_role.lambda[0].arn : var.lambda_role_arn
 
-  # optional
-  aws_region_code         = var.aws_region_code
-  fivetran_aws_account_id = var.fivetran_aws_account_id
-  lambda_role_arn         = var.lambda_role_arn
-  # Check on this whether it is refer to here or the fivetran_lambda module
-  lambda_source_dir  = "${path.module}/tracker"
-  lambda_output_path = "${path.module}/dist/tracker.zip"
-  script_env = {
-    RESOURCES_DATA = jsonencode(var.tracked_resources_data)
-    PERIOD         = "3600"
+  filename = data.archive_file.zip.output_path
+  # Used to trigger updates
+  source_code_hash = data.archive_file.zip.output_base64sha256
+  handler          = "index.handler"
+  # List of available runtimes: https://docs.aws.amazon.com/lambda/latest/dg/API_CreateFunction.html#SSS-CreateFunction-request-Runtime
+  runtime = "nodejs16.x"
+  timeout = 300
+
+  environment {
+    variables = {
+      RESOURCES_DATA = jsonencode(var.tracked_resources_data)
+      PERIOD         = "3600"
+    }
   }
+}
 
-  # for the backward compatibility of the previous version
-  connector_name = var.organisation == null ? null : "cloudwatch_metrics_${var.organisation}_${replace(var.aws_region_code, "/-/", "_")}"
+resource "aws_iam_role" "lambda" {
+  count = var.lambda_role_arn == null ? 1 : 0
+  name  = "fivetran_lambda_${var.fivetran_group_id}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${var.fivetran_aws_account_id}:root"
+        }
+        Condition = {
+          StringEquals = {
+            "sts:ExternalId" : "${var.fivetran_group_id}"
+          }
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "LambdaInvokePolicy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Sid      = "InvokePermission"
+          Effect   = "Allow"
+          Action   = ["lambda:InvokeFunction"]
+          Resource = "*"
+        }
+      ]
+    })
+  }
 }
 
 resource "aws_iam_role_policy_attachment" "fivetran_policy_for_lambda" {
--- cloudwatch-snowflake/variables.tf
+++ cloudwatch-snowflake/variables.tf
@@ -1,3 +1,15 @@
+variable "lambda_source_dir" {
+  type        = string
+  description = "Path to the directory containing the lambda function code"
+  default     = null
+}
+
+variable "lambda_output_path" {
+  type        = string
+  description = "Path to output the zipped lambda function code"
+  default     = null
+}
+
 variable "fivetran_group_id" {
   type        = string
   description = "Also know as external_id. Understand the group concept here: https://fivetran.com/docs/getting-started/powered-by-fivetran#createagroupusingtheui"
@@ -21,28 +33,9 @@ variable "aws_region_code" {
   description = "lambda's aws region"
 }
 
-variable "service_name" {
-  type        = string
-  description = "connector name shown on Fivetran UI, i.e. (service_name)_(project)_(env)_(aws_region_code)"
-  default     = null
-}
-
-variable "project" {
-  type        = string
-  description = "connector name shown on Fivetran UI, i.e. (service_name)_(project)_(env)_(aws_region_code)"
-  default     = null
-}
-
-variable "environment" {
-  type        = string
-  description = "connector name shown on Fivetran UI, i.e. (service_name)_(project)_(env)_(aws_region_code)"
-  default     = null
-}
-
 variable "organisation" {
   type        = string
   description = "connector name shown on Fivetran UI, i.e. cloudwatch_metrics_(organisation)"
-  default     = null
 }
 
 variable "tracked_resources_data" {
