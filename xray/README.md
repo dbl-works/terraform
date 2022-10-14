580fed403511958e387e23f5e87b6a3a9146202c
Lud <swiknaba@users.noreply.github.com>
2022-10-14 14:45:32 +0200

    adds IAM role to grant access to view X-Ray

--- xray/README.md
+++ xray/README.md
@@ -9,8 +9,12 @@ NOTE: The Xray group and sampling is shared within the whole region so this modu
 module "xray" {
   source = "github.com/dbl-works/terraform//xray?ref=v2021.08.24"
 
+  account_id = 1234567
+
   # optional
   response_time_threshold = 0.2
   duration_threshold      = 0.25
+  region                  = "eu-central-1"
+  name                    = "${var.region}-xray" # set a custom name if you want to follow a different naming convention
 }
 ```
--- xray/variables.tf
+++ xray/variables.tf
@@ -9,3 +9,20 @@ variable "duration_threshold" {
   default     = 0.12
   description = "Total request duration including all downstream calls."
 }
+
+variable "region" {
+  type        = string
+  default     = "eu-central-1"
+  description = "Region identifier"
+}
+
+variable "account_id" {
+  type        = string
+  description = "AWS account ID for the IAM access rules"
+}
+
+variable "name" {
+  type        = string
+  default     = null
+  description = "custom name if the auto-generated one is not desired"
+}
