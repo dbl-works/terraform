b43020ad3b77f2fe57de2d916e4f2004a2a5f959
Sam Kah Chiin <samkahchiin@gmail.com>
2022-10-25 14:37:39 +0800

    [WIP] Update setup cloudflare

--- stack/setup/cloudflare.tf
+++ stack/setup/cloudflare.tf
@@ -10,26 +10,36 @@ locals {
   )
 }
 
-# data "cloudflare_zone" "default" {
-#   name = var.domain
-# }
+data "cloudflare_zone" "default" {
+  name = var.domain
+}
 
-# # domain validation
-# resource "cloudflare_record" "validation" {
-#   # We assume that a read-replica stack runs on the same domain, hence we skip Cloudflare
-#   count = var.is_read_replica_on_same_domain ? 0 : length(local.distinct_domain_names)
+# domain validation
+resource "cloudflare_record" "validation" {
+  # We assume that a read-replica stack runs on the same domain, hence we skip Cloudflare
+  count = var.is_read_replica_on_same_domain ? 0 : length(local.distinct_domain_names)
 
-#   zone_id = data.cloudflare_zone.default.id
-#   name    = element(local.domain_validation_options, count.index)["resource_record_name"]
-#   type    = element(local.domain_validation_options, count.index)["resource_record_type"]
-#   # ACM DNS validation record returns the value with a trailing dot however the Cloudflare API trims it off.
-#   # https://github.com/cloudflare/terraform-provider-cloudflare/issues/154
-#   value = trimsuffix(element(local.domain_validation_options, count.index)["resource_record_value"], ".")
-# }
+  zone_id = data.cloudflare_zone.default.id
+  name    = element(local.domain_validation_options, count.index)["resource_record_name"]
+  type    = element(local.domain_validation_options, count.index)["resource_record_type"]
+  # ACM DNS validation record returns the value with a trailing dot however the Cloudflare API trims it off.
+  # https://github.com/cloudflare/terraform-provider-cloudflare/issues/154
+  value = trimsuffix(element(local.domain_validation_options, count.index)["resource_record_value"], ".")
+}
 
-# resource "aws_acm_certificate_validation" "default" {
-#   count = var.is_read_replica_on_same_domain ? 0 : 1
+resource "aws_acm_certificate_validation" "default" {
+  count = var.is_read_replica_on_same_domain ? 0 : 1
 
-#   certificate_arn         = aws_acm_certificate.main.arn
-#   validation_record_fqdns = cloudflare_record.validation.*.hostname
-# }
+  certificate_arn         = aws_acm_certificate.main.arn
+  validation_record_fqdns = cloudflare_record.validation.*.hostname
+}
+
+resource "cloudflare_zone_settings_override" "tls" {
+  name = data.cloudflare_zone.default.id
+
+  settings {
+    tls_1_3                  = "on"
+    automatic_https_rewrites = "on"
+    ssl                      = "strict"
+  }
+}
