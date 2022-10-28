573ff46751505a13aeec8600dac7342fa1290ba1
Sam Kah Chiin <samkahchiin@gmail.com>
2022-10-28 12:13:44 +0800

    [WIP] Add google analytics fivetran module

---
+++ fivetran/connectors/google_analytics/fivetran.tf
@@ -0,0 +1,46 @@
+locals {
+  name             = "google_analytics_${var.project}"
+  auth_credentials = var.auth == null ? [] : [var.auth]
+}
+
+resource "fivetran_connector" "google_analytics" {
+  group_id          = local.fivetran_group_id
+  service           = "google_analytics"
+  sync_frequency    = var.sync_frequency // min
+  paused            = false
+  pause_after_trial = false
+  run_setup_tests   = true
+
+  destination_schema {
+    name = local.name
+  }
+
+  config {
+    sync_mode        = "SpecificAccounts"
+    accounts         = var.google_account_ids
+    profiles         = var.google_profile_ids
+    timeframe_months = var.timeframe_months # TWELVE, SIX, ALL_TIME, TWENTY_FOUR, THREE
+
+    dynamic "reports" {
+      for_each = { for report in var.reports : report.table => report }
+
+      content {
+        table           = report.key
+        config_type     = report.value.config_type
+        prebuilt_report = report.value.prebuilt_report
+      }
+    }
+  }
+
+  dynamic "auth" {
+    for_each = { for cred in local.auth_credentials : cred.client_id => cred }
+
+    content {
+      client_access {
+        client_id     = auth.key
+        client_secret = auth.value.client_secret
+      }
+      refresh_token = auth.value.refresh_token
+    }
+  }
+}
