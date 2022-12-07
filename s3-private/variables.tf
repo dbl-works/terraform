838519683bf9921a1b52b8b98d3e08ebebcb9c00
Lud <swiknaba@users.noreply.github.com>
2022-12-07 18:05:12 +0700

    Fix syntax

--- s3-private/variables.tf
+++ s3-private/variables.tf
@@ -7,7 +7,7 @@ variable "kms_deletion_window_in_days" {
   description = "The waiting period, specified in number of days. After the waiting period ends, AWS KMS deletes the KMS key. If you specify a value, it must be between 7 and 30, inclusive. If you do not specify a value, it defaults to 30. If the KMS key is a multi-Region primary key with replicas, the waiting period begins when the last of its replica keys is deleted. Otherwise, the waiting period begins immediately."
 }
 
-policy_allow_listing_all_buckets = {
+variable "policy_allow_listing_all_buckets" {
   default = true
   type    = bool
 }
