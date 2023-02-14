1796541e75b1928cf56f2dc0a27f7c5b77057259
Sam Kah Chiin <samkahchiin@gmail.com>
2023-02-15 02:08:36 +0800

    [WIP] Add lifecycle policy rules to ecr modules

--- ecr/variables.tf
+++ ecr/variables.tf
@@ -1,3 +1,13 @@
 variable "project" {}
 
 variable "mutable" { default = false }
+
+variable "valid_days" {
+  type    = number
+  default = 3
+}
+
+variable "ecr_lifecycle_policy_rules" {
+  type    = list(object)
+  default = []
+}
