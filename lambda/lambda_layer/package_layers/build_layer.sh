e58e44d2e7df3434af5878dd6130c7088bfafb46
Sam Kah Chiin <samkahchiin@gmail.com>
2023-02-24 00:26:20 +0800

    [WIP] Fix pkg directory and version

--- lambda/lambda_layer/package_layers/build_layer.sh
+++ lambda/lambda_layer/package_layers/build_layer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-export PKG_DIR="python/lib/python3.9/site-packages/"
+export PKG_DIR="python"
 
 rm -rf ${PKG_DIR} && mkdir -p ${PKG_DIR}
 # --rm: Automatically remove the container when it exits
 # -v: Bind mount a volume
 # -w: Working directory inside the container
 # lambda/lambda: A sandboxed local environment that replicates the live AWS Lambda environment almost identically (https://hub.docker.com/r/lambci/lambda/)
-docker run --rm -v $(pwd):/layer -w /layer lambci/lambda:build-python3.7 \
+docker run --rm -v $(pwd):/layer -w /layer lambci/lambda:build-python3.9 \
 
 # Run this command in the docker images
 pip3 install -r requirements.txt --no-deps -t ${PKG_DIR}
