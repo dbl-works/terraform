2107d6ea1eb58b6539dec6a3554c79185c50a466
Sam Kah Chiin <samkahchiin@gmail.com>
2023-03-03 21:59:52 +0800

    [WIP] Add kms decrypt to general access

--- iam/iam-policy-for-taggable-resources/project-admins-policies.tf
+++ iam/iam-policy-for-taggable-resources/project-admins-policies.tf
@@ -20,28 +20,28 @@ data "aws_iam_policy_document" "admin" {
   }
 }
 
-data "aws_iam_policy_document" "secret_manager" {
-  statement {
-    sid = "AllowSecretAccessBasedOnTags"
-    actions = flatten([for resource in local.admin_resources : [
-      "${resource}:*",
-    ]])
+# data "aws_iam_policy_document" "secret_manager" {
+#   statement {
+#     sid = "AllowSecretAccessBasedOnTags"
+#     actions = flatten([for resource in local.admin_resources : [
+#       "${resource}:*",
+#     ]])
 
-    resources = ["*"]
+#     resources = ["*"]
 
-    condition {
-      test     = "StringEquals"
-      variable = "aws:ResourceTag/Environment"
-      values   = [var.environment]
-    }
+#     condition {
+#       test     = "StringEquals"
+#       variable = "aws:ResourceTag/Environment"
+#       values   = [var.environment]
+#     }
 
-    condition {
-      # Using StringLike here because currently tag cannot take multivalue
-      # We can only create a custom multivalue structure in the single value
-      # https://docs.aws.amazon.com/IAM/latest/UserGuide/id_tags.html
-      test     = "StringLike"
-      variable = "aws:ResourceTag/Project"
-      values   = ["&{aws:PrincipalTag/${var.environment}-admin-access-projects}"]
-    }
-  }
-}
+#     condition {
+#       # Using StringLike here because currently tag cannot take multivalue
+#       # We can only create a custom multivalue structure in the single value
+#       # https://docs.aws.amazon.com/IAM/latest/UserGuide/id_tags.html
+#       test     = "StringLike"
+#       variable = "aws:ResourceTag/Project"
+#       values   = ["&{aws:PrincipalTag/${var.environment}-admin-access-projects}"]
+#     }
+#   }
+# }
