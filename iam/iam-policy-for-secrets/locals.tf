e75fecf6cd297b086243edd3605ea65936eeae6f
Sam Kah Chiin <samkahchiin@gmail.com>
2023-03-06 13:46:03 +0800

    [WIP] Pass the right project name

--- iam/iam-policy-for-secrets/locals.tf
+++ iam/iam-policy-for-secrets/locals.tf
@@ -7,7 +7,7 @@ locals {
   admin_access_projects = flatten([
     for env, project_names in try(var.project_access["admin"], {}) : [
       for project_name in project_names : {
-        "name"        = "${project_name}-${env}"
+        "name"        = project_name
         "environment" = env
       }
     ]
