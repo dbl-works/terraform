1fffc1e76de586bcda8b9122b2c97f01383d0766
Sam Kah Chiin <samkahchiin@gmail.com>
2023-03-06 13:53:10 +0800

    [WIP] Use id instead of arn

--- iam/iam-policy-for-secrets/main.tf
+++ iam/iam-policy-for-secrets/main.tf
@@ -22,8 +22,8 @@ data "aws_iam_policy_document" "secrets" {
     ]])
 
     resources = concat(
-      data.aws_secretsmanager_secret.app.*.arn,
-      data.aws_secretsmanager_secret.terraform.*.arn
+      data.aws_secretsmanager_secret.app.*.id,
+      data.aws_secretsmanager_secret.terraform.*.id
     )
   }
 }
