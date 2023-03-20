be85e94d9a54cd60fdd7cbb6d95df9fbeff5212b
Lud <swiknaba@users.noreply.github.com>
2023-03-20 20:34:25 +0800

    typo

--- nat/nat.tf
+++ nat/nat.tf
@@ -41,7 +41,7 @@ resource "aws_route_table" "main" {
 # route tables for public subnet, e.g. if we want VPC peering between stacks without NATs
 # running the web process in a public subnet
 resource "aws_route_table_association" "main" {
-  for_each  = { for idx, subnet in var.subnet_public_ids : subnet => idx }
+  for_each  = { for idx, subnet in var.subnet_private_ids : subnet => idx }
   subnet_id = each.key
 
   route_table_id = aws_route_table.main[each.value].id
