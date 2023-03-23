ad1133a9a8bbb978142f33b6efdc64c7f8579835
Lud <swiknaba@users.noreply.github.com>
2023-03-23 12:21:31 +0100

    Fix Stack/Cloudflare is a list

--- cloudflare/outputs.tf
+++ cloudflare/outputs.tf
@@ -5,15 +5,15 @@ output "dnssec_instructions" {
 
 output "dnssec_algorithm" {
   description = "value of the DNSSEC algorithm, e.g. '13' which is 'ECDSA Curve P-256 with SHA-256'"
-  value       = cloudflare_zone_dnssec.main.algorithm
+  value       = cloudflare_zone_dnssec[*].main.algorithm
 }
 
 output "dnssec_key_type" {
   description = "key type of the DNSSEC record, e.g. '257 (KSK)'"
-  value       = cloudflare_zone_dnssec.main.key_type
+  value       = cloudflare_zone_dnssec[*].main.key_type
 }
 
 output "dnssec_public_key" {
   description = "public key of the DNSSEC record"
-  value       = cloudflare_zone_dnssec.main.public_key
+  value       = cloudflare_zone_dnssec[*].main.public_key
 }
