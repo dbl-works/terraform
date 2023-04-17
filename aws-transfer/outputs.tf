44832aeb0197c1586df5a359913797666cef88bd
Sam Kah Chiin <samkahchiin@gmail.com>
2023-04-17 15:21:01 +0800

    [WIP] Add kms arn as output

--- aws-transfer/outputs.tf
+++ aws-transfer/outputs.tf
@@ -5,3 +5,7 @@ output "endpoint" {
 output "s3_arn" {
   value = var.s3_bucket_name == null ? "" : module.s3-storage[0].arn
 }
+
+output "s3_kms_arn" {
+  value = var.s3_bucket_name == null ? "" : module.s3-storage[0].kms-key-arn
+}
