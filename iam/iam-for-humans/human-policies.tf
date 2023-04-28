53f21dd38fcb01544f6c332de8c44835b9a109ad
Sam Kah Chiin <samkahchiin@gmail.com>
2023-04-28 14:26:35 +0800

    [WIP] Update MFA policy

--- iam/iam-for-humans/human-policies.tf
+++ iam/iam-for-humans/human-policies.tf
@@ -10,66 +10,65 @@ resource "aws_iam_policy" "iam-humans-usage" {
   path        = "/"
   description = "Allow humans to manage their own credentials"
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "AllowViewAccountInfo",
-      "Effect": "Allow",
-      "Action": [
-        "iam:GetAccountPasswordPolicy",
-        "iam:GetAccountSummary",
-        "iam:ListVirtualMFADevices"
-      ],
-      "Resource": "*"
-    },
-    {
-      "Sid": "AllowManageOwnPasswords",
-      "Effect": "Allow",
-      "Action": [
-        "iam:ChangePassword",
-        "iam:GetUser"
-      ],
-      "Resource": "arn:aws:iam::*:user/$${aws:username}"
-    },
-    {
-      "Sid": "AllowManageOwnAccessKeys",
-      "Effect": "Allow",
-      "Action": [
-        "iam:CreateAccessKey",
-        "iam:DeleteAccessKey",
-        "iam:GetAccessKeyLastUsed",
-        "iam:ListAccessKeys",
-        "iam:UpdateAccessKey"
-      ],
-      "Resource": "arn:aws:iam::*:user/$${aws:username}"
-    },
-    {
-      "Sid": "AllowManageOwnVirtualMFADevice",
-      "Effect": "Allow",
-      "Action": [
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowViewAccountInfo",
+        Effect = "Allow",
+        Action = [
+          "iam:GetAccountPasswordPolicy",
+          "iam:GetAccountSummary",
+          "iam:ListVirtualMFADevices"
+        ],
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowManageOwnPasswords",
+        Effect = "Allow",
+        Action = [
+          "iam:ChangePassword",
+          "iam:GetUser"
+        ],
+        Resource = "arn:aws:iam::*:user/$${aws:username}"
+      },
+      {
+        Sid    = "AllowManageOwnAccessKeys",
+        Effect = "Allow",
+        Action = [
+          "iam:CreateAccessKey",
+          "iam:DeleteAccessKey",
+          "iam:GetAccessKeyLastUsed",
+          "iam:ListAccessKeys",
+          "iam:UpdateAccessKey"
+        ],
+        Resource = "arn:aws:iam::*:user/$${aws:username}"
+      },
+      {
+        Sid    = "AllowManageVirtualMFADevice",
+        Effect = "Allow",
+        Action = [
           "iam:CreateVirtualMFADevice",
           "iam:DeleteVirtualMFADevice"
-      ],
-      "Resource": "arn:aws:iam::*:mfa/$${aws:username}"
-    },
-    {
-      "Sid": "AllowManageOwnUserMFA",
-      "Effect": "Allow",
-      "Action": [
-        "iam:DeactivateMFADevice",
-        "iam:EnableMFADevice",
-        "iam:GetUser",
-        "iam:ListMFADevices",
-        "iam:ResyncMFADevice"
-      ],
-      "Resource": "arn:aws:iam::*:user/$${aws:username}"
-    }
-  ]
+        ],
+        "Resource" : "arn:aws:iam::*:mfa/*"
+      },
+      {
+        Sid    = "AllowManageOwnUserMFA",
+        Effect = "Allow",
+        Action = [
+          "iam:DeactivateMFADevice",
+          "iam:EnableMFADevice",
+          "iam:GetUser",
+          "iam:ListMFADevices",
+          "iam:ResyncMFADevice"
+        ],
+        Resource = "arn:aws:iam::*:user/$${aws:username}"
+      }
+    ]
+  })
 }
-EOF
-}
+
 resource "aws_iam_group_policy_attachment" "iam-humans-usage" {
   group      = "humans"
   policy_arn = aws_iam_policy.iam-humans-usage.arn
