7a2e2724c3010cba5f08426c521f47fa9174cc2d
Sam Kah Chiin <samkahchiin@gmail.com>
2023-05-18 17:18:36 +0800

    [WIP] Fix the naming

--- s3-public/s3.tf
+++ s3-public/s3.tf
@@ -11,16 +11,21 @@ module "s3" {
 resource "aws_s3_bucket_acl" "main-bucket-data-acl" {
   bucket = module.s3.id
   acl    = "public-read"
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.main,
+    aws_s3_bucket_public_access_block.main
+  ]
 }
 
-resource "aws_s3_bucket_ownership_controls" "example" {
+resource "aws_s3_bucket_ownership_controls" "main" {
   bucket = module.s3.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
+resource "aws_s3_bucket_public_access_block" "main" {
   bucket = module.s3.id
 
   block_public_acls       = false
