d003a23bbac4f4b99f3bc85c70d9693f88068d1e
Sam Kah Chiin <samkahchiin@gmail.com>
2023-07-27 16:20:18 +0800

    Update slack/ecs-deployment-failure/lambdas/main.rb
    Co-authored-by: Lud <lud@reinmiedl.com>

--- slack/ecs-deployment-failure/lambdas/main.rb
+++ slack/ecs-deployment-failure/lambdas/main.rb
@@ -7,7 +7,7 @@ def handler(event:, context:)
   puts "[INFO] #{event}"
   puts "[INFO] Context: #{context}"
   event_name = event.fetch('detail').fetch('eventName')
-  return unless deployment_failure?(event_name)
+  return unless event_name == FAILED_EVENT
 
   resource_name = event.fetch('resources').join(',')
   region = event.fetch('region')
