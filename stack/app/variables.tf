d87be3999410b21d1779854a8c098fc5acf9a67f
Sam Kah Chiin <samkahchiin@gmail.com>
2023-09-13 15:07:58 +0800

    [WIP] Add health check options

--- stack/app/variables.tf
+++ stack/app/variables.tf
@@ -321,6 +321,18 @@ variable "rds_final_snapshot_identifier" {
 
 # =============== ECS ================ #
 variable "health_check_path" { default = "/livez" }
+
+variable "health_check_options" {
+  type = object({
+    healthy_threshold   = optional(number, 2)  # The number of consecutive health checks successes required before considering an unhealthy target healthy.
+    unhealthy_threshold = optional(number, 5)  # The number of consecutive health check failures required before considering the target unhealthy. For Network Load Balancers, this value must be the same as the healthy_threshold.
+    timeout             = optional(number, 30) # The amount of time, in seconds, during which no response means a failed health check. For Application Load Balancers, the range is 2 to 120 seconds.
+    interval            = optional(number, 60) # The approximate amount of time, in seconds, between health checks of an individual target. Minimum value 5 seconds, Maximum value 300 seconds.
+    matcher             = optional(string, "200,204")
+  })
+  default = {}
+}
+
 variable "allow_internal_traffic_to_ports" {
   type    = list(string)
   default = []
