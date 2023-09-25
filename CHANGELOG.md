129ca86e2377b7a7fb4928507947b6726c793142
Sam Kah Chiin <samkahchiin@gmail.com>
2023-09-25 15:53:14 +0800

    Revert "stack/setup: prevent clickjacking (#256)"
    This reverts commit 8b97ae2a2fdeb3b8d6c7612a20317f56a769df7d.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,9 +7,6 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) 
 This project does not follow SemVer, since modules are independent of each other; thus, SemVer does not make sense. Changes are grouped per module.
 
 ## Unreleased
-### stack/setup
--  Prevent clickjacking. [#256](https://github.com/dbl-works/terraform/pull/256)
-
 ### ecs
 - Add cloudwatch event rules to monitor the state changes of container tasks. [#252](https://github.com/dbl-works/terraform/pull/252)
 
--- stack/setup/cloudflare.tf
+++ stack/setup/cloudflare.tf
@@ -33,25 +33,3 @@ resource "aws_acm_certificate_validation" "default" {
   certificate_arn         = aws_acm_certificate.main[0].arn
   validation_record_fqdns = cloudflare_record.validation.*.hostname
 }
-
-resource "cloudflare_ruleset" "clickjacking" {
-  zone_id     = data.cloudflare_zone.default.id
-  name        = "Clickjacking"
-  description = "Prevent clickjacking"
-  kind        = "zone"
-  phase       = "http_response_headers_transform"
-
-  rules {
-    action = "rewrite"
-    action_parameters {
-      headers {
-        name      = "X-Frame-Options"
-        operation = "set"
-        value     = "SAMEORIGIN"
-      }
-    }
-    expression  = "true"
-    description = "Clickjacking"
-    enabled     = true
-  }
-}
