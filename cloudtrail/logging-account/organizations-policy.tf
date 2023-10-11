eb30d9fbc7174f8af6333616147cb2a6dae1d49e
Sam Kah Chiin <samkahchiin@gmail.com>
2023-10-11 15:39:44 +0800

    [WIP] Fix count

--- cloudtrail/logging-account/organizations-policy.tf
+++ cloudtrail/logging-account/organizations-policy.tf
@@ -21,7 +21,7 @@ data "aws_iam_policy_document" "scp_policy" {
 resource "aws_organizations_policy_attachment" "default" {
   count = var.is_organization_trail ? 1 : 0
 
-  policy_id = aws_organizations_policy.scp_policy.id
+  policy_id = aws_organizations_policy.scp_policy[0].id
   # The unique identifier (ID) of the root, organizational unit, or account number that you want to attach the policy to.
   target_id = data.aws_caller_identity.current.account_id
 }
