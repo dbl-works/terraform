79ea4f6c2ce21bfc161c3e091fe45519fcf99a10
Sam Kah Chiin <samkahchiin@gmail.com>
2023-10-11 15:36:55 +0800

    [WIP] Disable scp when it is not an organization trail

--- cloudtrail/logging-account/organizations-policy.tf
+++ cloudtrail/logging-account/organizations-policy.tf
@@ -1,4 +1,6 @@
 resource "aws_organizations_policy" "scp_policy" {
+  count = var.is_organization_trail ? 1 : 0
+
   name        = "scp_cloudtrail"
   description = "This SCP prevents users or roles in any affected account from disabling a CloudTrail log, either directly as a command or through the console. "
   type        = "SERVICE_CONTROL_POLICY"
@@ -17,6 +19,8 @@ data "aws_iam_policy_document" "scp_policy" {
 }
 
 resource "aws_organizations_policy_attachment" "default" {
+  count = var.is_organization_trail ? 1 : 0
+
   policy_id = aws_organizations_policy.scp_policy.id
   # The unique identifier (ID) of the root, organizational unit, or account number that you want to attach the policy to.
   target_id = data.aws_caller_identity.current.account_id
