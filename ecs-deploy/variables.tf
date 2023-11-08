e49181ebd2766d63b0505bd3389eb69543709268
Lud <lud@reinmiedl.com>
2023-11-08 16:19:11 +0700

    terraform syntax -.-

--- ecs-deploy/variables.tf
+++ ecs-deploy/variables.tf
@@ -39,10 +39,10 @@ variable "ecs_fargate_log_mode" {
 
 variable "ephemeral_storage_size_in_gib" {
   type    = number
-  default = null
+  default = 20 # 20 is the default of AWS Fargate
 
   validation {
-    condition     = var.ephemeral_storage_size_in_gib == null || (var.ephemeral_storage_size_in_gib >= 21 && var.ephemeral_storage_size_in_gib <= 200)
+    condition     = var.ephemeral_storage_size_in_gib == 20 || (var.ephemeral_storage_size_in_gib >= 21 && var.ephemeral_storage_size_in_gib <= 200)
     error_message = "ephemeral_storage_size_in_gib must omitted to default to 20, or set to be between 21 and 200 (GiB)"
   }
 }
