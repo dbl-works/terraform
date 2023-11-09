509c34fa7453b2064f3e15f00e42928dfca79f1d
Sam Kah Chiin <samkahchiin@gmail.com>
2023-11-09 15:11:46 +0800

    [WIP] Upgrade cloudwatch kinesis to be used by v5

--- cloudwatch-kinesis/versions.tf
+++ cloudwatch-kinesis/versions.tf
@@ -1,9 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      # Some of the configuration (i.e. s3) is deprecated in version 5
-      version = ">= 4.0, < 5"
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
     }
   }
   required_version = ">= 1.0"
