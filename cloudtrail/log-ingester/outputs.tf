22501b0c9f8d49f30562dc9de00d5cadd87d637f
Lud <lud@reinmiedl.com>
2023-11-22 11:34:03 +0100

    output bucket arn, region, kms key id

--- cloudtrail/log-ingester/outputs.tf
+++ cloudtrail/log-ingester/outputs.tf
@@ -1,7 +1,21 @@
+# Bucket information
 output "s3_bucket_name" {
   value = module.s3-cloudtrail.bucket_name
 }
 
+output "s3_bucket_arn" {
+  value = module.s3-cloudtrail.bucket_arn
+}
+
+output "s3_bucket_region" {
+  value = data.aws_region.current.name
+}
+
+# KMS information
 output "s3_kms_arn" {
   value = module.s3-cloudtrail.kms-key-arn
+}
+
+output "s3_kms_key_id" {
+  value = module.s3-cloudtrail.kms-key-id
 }
