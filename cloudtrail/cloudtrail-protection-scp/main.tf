45aaef3e4f99fcbb55a6bed07f95c9b725f87649
Lud <lud@reinmiedl.com>
2023-11-22 11:29:34 +0100

    only protect trails going to the log ingester acount

--- cloudtrail/cloudtrail-protection-scp/main.tf
+++ cloudtrail/cloudtrail-protection-scp/main.tf
@@ -15,6 +15,15 @@ data "aws_iam_policy_document" "cloudtrail_protection_scp" {
       "cloudtrail:StopLogging",
       "cloudtrail:UpdateTrail",
     ]
+
+    # this ensures we only protect logs that are ingested by the log ingester account
+    # which allows to have additional trails within a single log-producer account that are not protected
+    condition {
+      test     = "StringLike"
+      variable = "cloudtrail:trailarn"
+      values   = ["arn:aws:cloudtrail:*:${var.log_ingester_account_id}:trail/*"]
+    }
+  }
 }
 
 resource "aws_organizations_policy_attachment" "default" {
