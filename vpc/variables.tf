00c6212760a2c985c5902ad3aba089b89e9f3af9
Sam Kah Chiin <samkahchiin@gmail.com>
2023-11-29 22:48:39 +0800

    [WIP] Remove region and account_id from vpc variables

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -3,7 +3,4 @@ variable "availability_zones" {
 }
 variable "environment" {}
 variable "project" {}
-variable "account_id" {}
-
-variable "region" { default = "eu-central-1" }
 variable "cidr_block" { default = "10.0.0.0/16" }
