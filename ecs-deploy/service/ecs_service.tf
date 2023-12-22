4c4f51b5f2f2623d812d254c866401173ad97cb3
Sam Kah Chiin <samkahchiin@gmail.com>
2023-12-22 21:15:44 +0800

    [WIP] Add back missing launch_type

--- ecs-deploy/service/ecs_service.tf
+++ ecs-deploy/service/ecs_service.tf
@@ -52,6 +52,7 @@ resource "aws_ecs_service" "main" {
   cluster         = data.aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.main.arn
   desired_count   = var.desired_count
+  launch_type     = "FARGATE"
 
   deployment_circuit_breaker {
     enable   = var.deployment_circuit_breaker.enable
