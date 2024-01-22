07bc51b47d31c0d1754cc446b98a48173cba220d
Lud <lud@reinmiedl.com>
2024-01-22 11:06:20 +0100

    leave a suggestion

--- circleci-token-rotator/secrets.tf
+++ circleci-token-rotator/secrets.tf
@@ -5,6 +5,6 @@ module "secrets" {
   environment = local.environment
 
   # Optional
-  application = "circleci"
+  application = "circleci" # @NOTE: should we instead rename the "terraform" vault into "infra" and use that? that will reduce the number of vaults we ever need to 2: 1 for the app, 1 for all other infra stuff
   description = "Secrets for rotating CircleCI token"
 }
