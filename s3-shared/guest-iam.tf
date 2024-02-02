61a24a0c0ac33d36f4cedfc5e52d5c777c9ec4ea
Lud <lud@reinmiedl.com>
2024-02-02 18:27:52 +0100

    no need to list acl

--- s3-shared/guest-iam.tf
+++ s3-shared/guest-iam.tf
@@ -14,7 +14,6 @@ data "aws_iam_policy_document" "s3" {
     actions = [
       "s3:GetBucketLocation",
       "s3:GetObject",
-      "s3:GetObjectAcl",
       "s3:GetObjectTagging",
       "s3:GetObjectVersion",
       "s3:ListBucket",
