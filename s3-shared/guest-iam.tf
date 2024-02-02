f82f5bd3cd7d51634db83c9b018cd3a944c51080
Lud <lud@reinmiedl.com>
2024-02-02 17:28:28 +0100

    less permissions

--- s3-shared/guest-iam.tf
+++ s3-shared/guest-iam.tf
@@ -14,19 +14,15 @@ data "aws_iam_policy_document" "s3" {
     actions = [
       "s3:GetBucketAcl",
       "s3:GetBucketLocation",
-      "s3:GetBucketVersioning",
       "s3:GetObject",
       "s3:GetObjectAcl",
       "s3:GetObjectTagging",
       "s3:GetObjectVersion",
       "s3:ListBucket",
-      "s3:ListBucketVersions",
-      "s3:PutBucketVersioning",
       "s3:PutObject",
       "s3:PutObjectAcl",
       "s3:PutObjectTagging",
-      "s3:DeleteObject",
-      "s3:DeleteObjectVersion"
+      "s3:DeleteObject"
     ]
     resources = [
       module.s3.arn,
