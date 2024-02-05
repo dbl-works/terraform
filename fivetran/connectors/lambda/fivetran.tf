fb7d2bda5588d1338ac685e94770dc1b980b13f1
Lud <lud@reinmiedl.com>
2024-02-05 10:52:03 +0100

    adjust fivetran connector to breaking changes

--- fivetran/connectors/lambda/fivetran.tf
+++ fivetran/connectors/lambda/fivetran.tf
@@ -1,10 +1,7 @@
 resource "fivetran_connector" "lambda" {
-  group_id          = var.fivetran_group_id
-  service           = "aws_lambda"
-  sync_frequency    = var.sync_frequency // min
-  paused            = false
-  pause_after_trial = false
-  run_setup_tests   = true
+  group_id        = var.fivetran_group_id
+  service         = "aws_lambda"
+  run_setup_tests = true
 
   destination_schema {
     name = var.connector_name == null ? local.function_name : var.connector_name
@@ -15,4 +12,17 @@ resource "fivetran_connector" "lambda" {
     role_arn = var.lambda_role_arn
     region   = var.aws_region_code
   }
+}
+
+
+resource "fivetran_connector_schedule" "lambda" {
+  connector_id = fivetran_connector.lambda.id
+
+  sync_frequency  = var.sync_frequency
+  daily_sync_time = "03:00"
+
+  paused            = false
+  pause_after_trial = false
+
+  schedule_type = "auto"
 }
