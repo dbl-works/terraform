17a23526c17f9724a3ab5059659baf3ae56dcc57
Sam Kah Chiin <samkahchiin@gmail.com>
2024-02-15 16:29:44 +0800

    [WIP] REmove nat

--- multi-stack/app/nat.tf
+++
@@ -1,9 +0,0 @@
-module "nat" {
-  source             = "../../nat"
-  project            = var.project
-  environment        = var.environment
-  vpc_id             = module.vpc.id
-  subnet_public_ids  = module.vpc.subnet_public_ids
-  subnet_private_ids = module.vpc.subnet_private_ids
-  public_ips         = var.public_ips
-}
