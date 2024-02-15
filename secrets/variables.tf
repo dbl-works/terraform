cd438ff8f54590729a6f9cd143d47431ebb493cc
Sam Kah Chiin <samkahchiin@gmail.com>
2024-02-15 18:20:31 +0800

    [WIP] Remove comments

--- secrets/variables.tf
+++ secrets/variables.tf
@@ -5,7 +5,6 @@ variable "create_kms_key" {
   type    = bool
   default = true
 }
-# TODO: @sam, add condition, either one need to be true or with value
 
 variable "kms_key_id" {
   type    = string
