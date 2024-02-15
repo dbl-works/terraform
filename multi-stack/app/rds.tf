5569eb59888dc4c59221f865ea7af146b42e9ba3
Sam Kah Chiin <samkahchiin@gmail.com>
2024-02-15 15:52:40 +0800

    [WIP] Tidy out variables

--- multi-stack/app/rds.tf
+++ multi-stack/app/rds.tf
@@ -9,7 +9,7 @@ module "rds-kms-key" {
 
   # Optional
   deletion_window_in_days = var.kms_deletion_window_in_days
-  multi_region            = var.rds_multi_region_kms_key
+  multi_region            = var.rds_config.multi_region_kms_key
 }
 
 module "rds" {
@@ -22,33 +22,33 @@ module "rds" {
   project     = var.project
   environment = var.environment
   vpc_id      = module.vpc.id
-  password    = var.rds_is_read_replica ? null : local.credentials.db_root_password
-  kms_key_arn = var.rds_master_db_kms_key_arn == null ? module.rds-kms-key.arn : var.rds_master_db_kms_key_arn
+  password    = local.credentials.db_root_password
+  kms_key_arn = module.rds-kms-key.arn
   subnet_ids  = module.vpc.subnet_private_ids
 
   allow_from_security_groups = [module.ecs.ecs_security_group_id]
 
   # optional
-  username                        = var.rds_is_read_replica ? null : local.credentials.db_username
-  instance_class                  = var.rds_instance_class
-  engine_version                  = var.rds_engine_version
-  allocated_storage               = var.rds_allocated_storage
-  multi_az                        = var.rds_multi_az == null ? var.environment == "production" : var.rds_multi_az
-  master_db_instance_arn          = var.rds_master_db_instance_arn
-  is_read_replica                 = var.rds_is_read_replica
+  username                        = local.credentials.db_username
+  instance_class                  = var.rds_config.instance_class
+  engine_version                  = var.rds_config.config.engine_version
+  allocated_storage               = var.rds_config.allocated_storage
+  multi_az                        = var.rds_config.multi_az == null ? var.environment == "production" : var.rds_config.multi_az
+  master_db_instance_arn          = null
+  is_read_replica                 = false
   regional                        = true
   region                          = var.region
-  name                            = var.rds_name
-  identifier                      = var.rds_identifier
-  allow_from_cidr_blocks          = var.rds_allow_from_cidr_blocks
-  subnet_group_name               = var.rds_subnet_group_name
-  delete_automated_backups        = var.rds_delete_automated_backups
-  skip_final_snapshot             = var.rds_skip_final_snapshot
-  final_snapshot_identifier       = var.rds_final_snapshot_identifier
-  log_min_duration_statement      = var.rds_log_min_duration_statement
-  log_retention_period            = var.rds_log_retention_period
-  log_min_error_statement         = var.rds_log_min_error_statement
-  ca_cert_identifier              = var.rds_ca_cert_identifier
-  storage_autoscaling_upper_limit = var.rds_storage_autoscaling_upper_limit
-  backup_retention_period         = var.rds_backup_retention_period
+  name                            = var.rds_config.config.name
+  identifier                      = var.rds_config.config.identifier
+  allow_from_cidr_blocks          = var.rds_config.allow_from_cidr_blocks
+  subnet_group_name               = var.rds_config.subnet_group_name
+  delete_automated_backups        = var.rds_config.delete_automated_backups
+  skip_final_snapshot             = var.rds_config.skip_final_snapshot
+  final_snapshot_identifier       = var.rds_config.final_snapshot_identifier
+  log_min_duration_statement      = var.rds_config.log_min_duration_statement
+  log_retention_period            = var.rds_config.log_retention_period
+  log_min_error_statement         = var.rds_config.log_min_error_statement
+  ca_cert_identifier              = var.rds_config.ca_cert_identifier
+  storage_autoscaling_upper_limit = var.rds_config.storage_autoscaling_upper_limit
+  backup_retention_period         = var.rds_config.backup_retention_period
 }
--- multi-stack/app/variables.tf
+++ multi-stack/app/variables.tf
@@ -57,22 +57,6 @@ variable "region" {
   default = "eu-central-1"
 }
 
-# =============== Certificate Manager ================ #
-variable "domain_name" {
-  type = string
-}
-
-variable "skip_cloudflare" {
-  type    = bool
-  default = false
-}
-
-variable "certificate_arn" {
-  type    = string
-  default = null
-}
-# =============== Certificate Manager ================ #
-
 # =============== Cloudflare ================ #
 variable "tls_settings" {
   type = object({
@@ -83,17 +67,11 @@ variable "tls_settings" {
   })
   default = null
 }
-# =============== Cloudflare ================ #
-
 # =============== KMS ================ #
 variable "kms_deletion_window_in_days" {
   type = number
 }
 
-variable "grant_access_to_kms_arns" {
-  type    = list(string)
-  default = []
-}
 # =============== KMS ================ #
 
 # =============== NAT ================ #
@@ -113,11 +91,6 @@ variable "vpc_cidr_block" {
   type = string
 }
 
-variable "vpc_peering" {
-  type    = bool
-  default = false
-}
-
 # CIDR blocks from all regions that are not this region
 variable "remote_cidr_blocks" {
   type    = list(string)
@@ -127,11 +100,6 @@ variable "remote_cidr_blocks" {
 
 
 # =============== Elasticache ================ #
-variable "skip_elasticache" {
-  type    = bool
-  default = false
-}
-
 variable "elasticache_transit_encryption_enabled" {
   type        = bool
   default     = true
@@ -218,264 +186,25 @@ variable "elasticache_automatic_failover_enabled" {
   default = true
 }
 # =============== Elasticache ================ #
-
-# =============== RDS ================ #
-variable "skip_rds" {
-  type    = bool
-  default = false
-}
-
-variable "rds_name" {
-  type    = string
-  default = null
-}
-
-variable "rds_identifier" {
-  type    = string
-  default = null
-}
-
-# set the key for the master DB to multi-region if you have read replicas in other regions
-variable "rds_multi_region_kms_key" {
-  type    = bool
-  default = false
-}
-variable "rds_is_read_replica" {
-  type    = bool
-  default = false
-}
-variable "rds_master_db_instance_arn" {
-  default = null
-  type    = string
-}
-variable "rds_master_db_vpc_cidr_block" {
-  default = null
-  type    = string
-}
-variable "rds_master_db_region" {
-  type    = string
-  default = null
-}
-variable "rds_master_db_vpc_id" {
-  type    = string
-  default = null
-}
-variable "rds_master_db_kms_key_arn" {
-  type    = string
-  default = null
-}
-variable "rds_master_nat_route_table_ids" {
-  type    = list(string)
-  default = []
-}
-variable "rds_instance_class" {
-  type    = string
-  default = "db.t3.micro"
-}
-variable "rds_engine_version" {
-  type    = string
-  default = "16"
-}
-variable "rds_allocated_storage" {
-  type    = number
-  default = 10
-}
-variable "rds_allow_from_cidr_blocks" {
-  type    = list(string)
-  default = []
-}
-
-variable "rds_subnet_group_name" {
-  type    = string
-  default = null
-}
-
-variable "rds_backup_retention_period" {
-  default = 7
-  type    = number
-}
-
-variable "rds_storage_autoscaling_upper_limit" {
-  type    = number
-  default = 20
-}
-
-variable "rds_multi_az" {
-  type        = bool
-  description = "if no value is passed, multi-az will be set to true if the environment is production"
-  default     = null
-}
-
-variable "rds_delete_automated_backups" {
-  type    = bool
-  default = true
-}
-
-variable "rds_skip_final_snapshot" {
-  type    = bool
-  default = false
-}
-
-variable "rds_final_snapshot_identifier" {
-  type    = string
-  default = null
-}
-
-variable "rds_log_min_duration_statement" {
-  type        = number
-  default     = -1
-  description = "Used to log SQL statements that run longer than a specified duration of time (in ms)."
-}
-
-variable "rds_log_retention_period" {
-  type        = number
-  default     = 1440
-  description = "Controls how long automatic RDS log files are retained before being deleted (in min, must be between 1440-10080 (1-7 days)."
-
-  validation {
-    condition     = var.rds_log_retention_period >= 1440 && var.rds_log_retention_period <= 10080
-    error_message = "Log retention period must be between 1440-10080 (1-7 days)."
-  }
-}
-
-variable "rds_log_min_error_statement" {
-  type        = string
-  default     = "panic"
-  description = "Controls which SQL statements that cause an error condition are recorded in the server log."
-
-  validation {
-    condition     = contains(["debug5", "debug4", "debug3", "debug2", "debug1", "info", "notice", "warning", "error", "log", "fatal", "panic"], var.rds_log_min_error_statement)
-    error_message = "The valid values are [debug5, debug4, debug3, debug2, debug1, info, notice, warning, error, log, fatal, panic]"
-  }
-}
-
-variable "rds_ca_cert_identifier" {
-  default     = "rds-ca-ecc384-g1"
-  type        = string
-  description = "The identifier of the CA certificate for the DB instance."
-}
-# =============== RDS ================ #
-
-# =============== ECS ================ #
-variable "health_check_path" { default = "/livez" }
-
-variable "enable_container_insights" {
-  type    = bool
-  default = false
-}
-
-variable "health_check_options" {
+variable "rds_config" {
   type = object({
-    healthy_threshold   = optional(number, 2)  # The number of consecutive health checks successes required before considering an unhealthy target healthy.
-    unhealthy_threshold = optional(number, 5)  # The number of consecutive health check failures required before considering the target unhealthy. For Network Load Balancers, this value must be the same as the healthy_threshold.
-    timeout             = optional(number, 30) # The amount of time, in seconds, during which no response means a failed health check. For Application Load Balancers, the range is 2 to 120 seconds.
-    interval            = optional(number, 60) # The approximate amount of time, in seconds, between health checks of an individual target. Minimum value 5 seconds, Maximum value 300 seconds.
-    matcher             = optional(string, "200,204")
+    name                            = optional(string, null)
+    identifier                      = optional(string, null)
+    multi_region_kms_key            = optional(bool, false)
+    instance_class                  = optional(string, "db.t3.micro")
+    engine_version                  = optional(string, "16")
+    allocated_storage               = optional(number, 10)
+    allow_from_cidr_blocks          = optional(list(string), [])
+    subnet_group_name               = optional(string, null)
+    backup_retention_period         = optional(7, number)
+    storage_autoscaling_upper_limit = optional(number, 20)
+    multi_az                        = optional(bool, null)
+    delete_automated_backups        = optional(bool, true)
+    skip_final_snapshot             = optional(bool, false)
+    final_snapshot_identifier       = optional(string, null)
+    log_min_duration_statement      = optional(number, -1)
+    log_retention_period            = optional(number, 1440)
+    log_min_error_statement         = optional(string, "panic")
+    ca_cert_identifier              = optional("rds-ca-ecc384-g1", string)
   })
-  default = {}
-}
-
-variable "monitored_service_groups" {
-  type        = list(string)
-  default     = ["service:web"]
-  description = "ECS service groups to monitor STOPPED containers."
-}
-
-variable "keep_alive_timeout" {
-  type    = number
-  default = 60
-  validation {
-    condition     = var.keep_alive_timeout >= 60 && var.keep_alive_timeout <= 4000
-    error_message = "keep_alive_timeout must be between 60 and 4000"
-  }
-}
-
-# variable "alb_listener_rules" {
-#   type = list(object({
-#     priority         = string
-#     type             = string
-#     target_group_arn = string
-#     path_pattern     = optional(list(string), [])
-#   }))
-#   default = []
-# }
-
-variable "service_discovery_enabled" {
-  type    = bool
-  default = true
-}
-# =============== ECS ================ #
-
-# =============== Cloudwatch ================ #
-variable "cloudwatch_sns_topic_arns" {
-  type    = list(string)
-  default = []
-}
-
-variable "metric_period" {
-  type    = number
-  default = 60
-}
-
-variable "alarm_period" {
-  type    = number
-  default = 300
-}
-
-variable "alarm_evaluation_periods" {
-  type        = number
-  default     = 1
-  description = "The number of periods over which data is compared to the specified threshold."
-}
-
-variable "cloudwatch_custom_metrics" {
-  type    = list(any)
-  default = []
-}
-
-variable "cloudwatch_elasticache_names" {
-  type        = list(string)
-  default     = []
-  description = "Name of the elasticache cluster that needs to be monitored"
-}
-
-variable "cloudwatch_database_identifiers" {
-  type        = list(string)
-  default     = []
-  description = "Identifiers of the databases that needs to be monitored"
-}
-
-variable "cloudwatch_alb_arn_suffixes" {
-  type        = list(string)
-  default     = []
-  description = "Name of the load balancers that needs to be monitored"
-}
-
-variable "cloudwatch_cluster_names" {
-  type        = list(string)
-  default     = []
-  description = "Name of the ECS cluster that needs to be monitored"
-}
-
-# https://aws.amazon.com/rds/instance-types/
-variable "db_instance_class_memory_in_gb" {
-  type        = number
-  default     = null
-  description = "Optional. Used for calculating the cloudwatch alarm threshold"
-}
-
-variable "datapoints_to_alarm" {
-  type    = number
-  default = 1
-}
-
-variable "cloudwatch_logs_retention_in_days" {
-  type    = number
-  default = 90
-}
-# =============== Cloudwatch ================ #
-
-locals {
-  default_project_name = keys(var.project_settings)[0]
-  name                 = "${var.project}-${var.environment}"
 }
