8be0387fb63fb474a5cb5ed63722d3b336d40e99
Lud <lud@reinmiedl.com>
2024-02-20 09:34:54 +0100

    multi-cloud init: move all AWS resources to aws subdir (#312)
    * move to aws subfolder
    
    Co-Authored-By: Sam Kah Chiin <15337955+samkahchiin@users.noreply.github.com>
    
    * fix reference to cloudflare
    
    Co-Authored-By: Sam Kah Chiin <15337955+samkahchiin@users.noreply.github.com>
    
    * fix: make database roles cloud independent
    
    Co-Authored-By: Sam Kah Chiin <15337955+samkahchiin@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sam Kah Chiin <15337955+samkahchiin@users.noreply.github.com>

--- cloudflare/versions.tf
+++ cloudflare/versions.tf
@@ -4,11 +4,6 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = ">= 3.0"
     }
-
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0"
-    }
   }
   required_version = ">= 1.0"
 }
--- fivetran/connectors/lambda/versions.tf
+++ fivetran/connectors/lambda/versions.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0"
-    }
     fivetran = {
       source  = "fivetran/fivetran"
       version = "~> 1.0"
