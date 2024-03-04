cbc019d48291c22c2d068f5e6298a23d93e46d50
Sam Kah Chiin <samkahchiin@gmail.com>
2024-03-04 17:23:11 +0800

    [WIP] Add key vault azure

---
+++ azure/stack/key-vault.tf
@@ -0,0 +1,57 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "main" {
+  name                = local.name
+  location            = var.region
+  resource_group_name = var.resource_group_name
+  sku_name            = var.key_vault_config.sku_name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+
+  access_policy {
+    tenant_id = azurerm_user_assigned_identity.main.tenant_id
+    object_id = azurerm_user_assigned_identity.main.principal_id
+
+    key_permissions = [
+      "Get",
+      "Decrypt"
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+
+  soft_delete_retention_days = var.key_vault_config.retention_in_days
+  tags                       = local.default_tags
+}
+
+resource "azurerm_key_vault_key" "main" {
+  name         = local.name
+  key_vault_id = data.azurerm_key_vault.main.id
+  key_type     = var.key_vault_config.key_type
+  key_size     = var.key_vault_config.key_size
+
+  # TODO: Figure out what permission is needed
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
+
+  rotation_policy {
+    automatic {
+      time_before_expiry = "P${var.key_vault_config.rotate_before_expiry_in_days}D"
+    }
+
+    expire_after = "P${var.key_vault_config.expired_in_days}D"
+  }
+
+  tags = local.default_tags
+}
