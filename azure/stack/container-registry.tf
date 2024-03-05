5f5403ab2fb5cf011dbef1e83b2fc9923d7e9f03
Sam Kah Chiin <samkahchiin@gmail.com>
2024-03-06 01:44:10 +0800

    [WIP] UPdate container registry

--- azure/stack/container-registry.tf
+++ azure/stack/container-registry.tf
@@ -1,11 +1,15 @@
 module "container-registry" {
   source = "../container-registry"
 
-  resource_group_name        = var.resource_group_name
-  environment                = var.environment
-  region                     = var.region
-  project                    = var.project
-  user_assigned_identity_ids = [azurerm_user_assigned_identity.main.id]
-  encryption_client_id       = azurerm_user_assigned_identity.main.client_id
-  retention_in_days          = var.retention_in_days
+  name                          = var.container_registry_config.name
+  resource_group_name           = var.resource_group_name
+  environment                   = var.environment
+  region                        = var.region
+  project                       = var.project
+  sku                           = var.container_registry_config.sku
+  user_assigned_identity_ids    = [azurerm_user_assigned_identity.main.id]
+  retention_in_days             = var.container_registry_config.retention_in_days
+  encryption_client_id          = azurerm_user_assigned_identity.main.client_id
+  key_vault_key_id              = azurerm_key_vault_key.main.id
+  public_network_access_enabled = var.container_registry_config.public_network_access_enabled
 }
