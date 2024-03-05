03f8ea6bd2ceeb4b7cd921ebf0a21b77c6c07e34
Sam Kah Chiin <samkahchiin@gmail.com>
2024-03-06 01:44:26 +0800

    [WIP] Fix key vault

--- azure/stack/key-vault.tf
+++ azure/stack/key-vault.tf
@@ -7,31 +7,32 @@ resource "azurerm_key_vault" "main" {
   sku_name            = var.key_vault_config.sku_name
   tenant_id           = data.azurerm_client_config.current.tenant_id
 
-  access_policy {
-    tenant_id = azurerm_user_assigned_identity.main.tenant_id
-    object_id = azurerm_user_assigned_identity.main.principal_id
-
-    key_permissions = [
-      "Get",
-      "Decrypt"
-    ]
-
-    secret_permissions = [
-      "Get",
-    ]
-
-    storage_permissions = [
-      "Get",
-    ]
-  }
-
   soft_delete_retention_days = var.key_vault_config.retention_in_days
   tags                       = local.default_tags
 }
 
+resource "azurerm_key_vault_access_policy" "main" {
+  key_vault_id = azurerm_key_vault.main.id
+  tenant_id    = azurerm_user_assigned_identity.main.tenant_id
+  object_id    = azurerm_user_assigned_identity.main.principal_id
+
+  key_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get",
+    "Set",
+  ]
+
+  storage_permissions = [
+    "Get",
+  ]
+}
+
 resource "azurerm_key_vault_key" "main" {
   name         = local.name
-  key_vault_id = data.azurerm_key_vault.main.id
+  key_vault_id = azurerm_key_vault.main.id
   key_type     = var.key_vault_config.key_type
   key_size     = var.key_vault_config.key_size
 
@@ -50,8 +51,11 @@ resource "azurerm_key_vault_key" "main" {
       time_before_expiry = "P${var.key_vault_config.rotate_before_expiry_in_days}D"
     }
 
-    expire_after = "P${var.key_vault_config.expired_in_days}D"
+    expire_after         = "P${var.key_vault_config.expired_in_days}D"
+    notify_before_expiry = "P${var.key_vault_config.notify_before_expiry}D"
   }
 
   tags = local.default_tags
 }
+
+
