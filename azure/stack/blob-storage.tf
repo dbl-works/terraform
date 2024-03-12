9d1487237381245ca352118e180c742e327a2372
Sam Kah Chiin <samkahchiin@gmail.com>
2024-03-12 16:08:14 +0800

    [WIP] Add blob storage module

---
+++ azure/stack/blob-storage.tf
@@ -0,0 +1,18 @@
+module "blob-storage" {
+  for_each = var.blob_storage_config
+
+  source = "../blob-storage"
+
+  resource_group_name             = var.resource_group_name
+  name                            = each.key
+  region                          = var.region
+  project                         = var.project
+  environment                     = var.environment
+  container_access_type           = each.value.container_access_type
+  account_kind                    = each.value.account_kind
+  account_tier                    = each.value.account_tier
+  account_replication_type        = each.value.account_replication_type
+  public_network_access_enabled   = each.value.public_network_access_enabled
+  allow_nested_items_to_be_public = each.value.allow_nested_items_to_be_public
+  versioning_enabled              = each.value.versioning_enabled
+}
