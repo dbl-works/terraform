9562c03bbbb8d1139e0f94d6b5adbc38ffe33640
Sam Kah Chiin <samkahchiin@gmail.com>
2024-03-18 19:59:57 +0800

    [WIP] Add stakc setup module

---
+++ azure/stack/setup/user-identity.tf
@@ -0,0 +1,21 @@
+resource "azurerm_user_assigned_identity" "main" {
+  resource_group_name = var.resource_group_name
+  location            = var.region
+
+  name = local.default_name
+
+  tags = var.default_tags
+}
+
+### Output
+output "user_assigned_identity_id" {
+  value = azurerm_user_assigned_identity.main.id
+}
+
+output "user_assigned_identity_tenant_id" {
+  value = azurerm_user_assigned_identity.main.tenant_id
+}
+
+output "user_assigned_identity_principal_id" {
+  value = azurerm_user_assigned_identity.main.principal_id
+}
