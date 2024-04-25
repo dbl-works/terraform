b38a448c5db91a1b42a5e3adeca9533af93f50f2
Sam Kah Chiin <samkahchiin@gmail.com>
2024-04-25 13:07:33 +0800

    [WIP] Remove defender from the stack because it require owner access

--- azure/stack/app/security.tf
+++
@@ -1,8 +0,0 @@
-module "security" {
-  source = "../../security"
-
-  count = var.enable_defender ? 1 : 0
-
-  devop_email      = var.security_config.devop_email
-  alerts_to_admins = var.security_config.alerts_to_admins
-}
