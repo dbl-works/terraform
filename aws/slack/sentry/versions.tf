1b259e33f3abe559cd47cd8e5b8b04ae9bf72fc4
Sam Kah Chiin <samkahchiin@gmail.com>
2024-05-13 20:45:46 +0900

    [WIP] Set conditions and filters and actions as string instead of array

--- aws/slack/sentry/versions.tf
+++ aws/slack/sentry/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     sentry = {
       source  = "jianyuan/sentry"
-      version = ">= 0.11.0"
+      version = ">= 0.12.0"
     }
   }
   required_version = ">= 1.0"
