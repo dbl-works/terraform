7b72d5df8ad993f5c96e7ea1ff5875cf4c0460ba
Lud <lud@reinmiedl.com>
2026-03-31 20:45:32 +0800

    chore: address warning aws_region.name is deprecated (use .id)

--- aws/ecs/locals.tf
+++ aws/ecs/locals.tf
@@ -1,6 +1,6 @@
 data "aws_region" "current" {}
 
 locals {
-  region = data.aws_region.current.name
+  region = data.aws_region.current.id
   name   = var.name != null ? var.name : "${var.project}-${var.environment}${var.regional ? "-${local.region}" : ""}"
 }
